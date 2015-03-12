06df6358d4acbec695f9f95d2626a4cee3cdde20
duftler <duftler@google.com>
2015-03-12 18:05:04 -0400

    Update google smoke test to reflect renaming of deploy stage to linearDeploy.

--- orca-smoke-test/src/integrationTest/groovy/com/netflix/spinnaker/orca/smoke/gce/OrcaSmokeGoogleSpec.groovy
+++ orca-smoke-test/src/integrationTest/groovy/com/netflix/spinnaker/orca/smoke/gce/OrcaSmokeGoogleSpec.groovy
@@ -123,7 +123,7 @@ class OrcaSmokeGoogleSpec extends Specification {
       name       : "my-pipeline",
       stages     : [
         [
-          type         : "deploy",
+          type         : "linearDeploy",
           providerType : "gce",
           zone         : "us-central1-b",
           image        : "debian-7-wheezy-v20141021",
