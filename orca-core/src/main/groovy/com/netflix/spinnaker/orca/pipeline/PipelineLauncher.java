02103a59a681c8df865d41353460ad6088573bc5
Rob Fletcher <rfletcher@netflix.com>
2016-04-14 12:48:30 -0700

    a really unsatisfatory implementation of building task graph

---
+++ orca-core/src/main/groovy/com/netflix/spinnaker/orca/pipeline/PipelineLauncher.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2016 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netflix.spinnaker.orca.pipeline;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.spinnaker.orca.pipeline.model.Pipeline;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+import static java.lang.Boolean.parseBoolean;
+
+@Component
+class PipelineLauncher extends ExecutionLauncher<Pipeline> {
+
+  private final PipelineStartTracker startTracker;
+
+  @Autowired(required = false)
+  public PipelineLauncher(ObjectMapper objectMapper,
+                          @Qualifier("instanceInfo") InstanceInfo currentInstance,
+                          ExecutionRunner runner,
+                          Collection<StageDefinitionBuilder> stageDefinitionBuilders) {
+    this(objectMapper, currentInstance, runner, stageDefinitionBuilders, null);
+  }
+
+  @Autowired(required = false)
+  public PipelineLauncher(ObjectMapper objectMapper,
+                          @Qualifier("instanceInfo") InstanceInfo currentInstance,
+                          ExecutionRunner runner,
+                          Collection<StageDefinitionBuilder> stageDefinitionBuilders,
+                          PipelineStartTracker startTracker) {
+    super(objectMapper, currentInstance, runner, stageDefinitionBuilders);
+    this.startTracker = startTracker;
+  }
+
+  @SuppressWarnings("unchecked")
+  @Override protected Pipeline parse(String configJson) throws IOException {
+    // TODO: can we not just annotate the class properly to avoid all this?
+    Map<String, Serializable> config = objectMapper.readValue(configJson, Map.class);
+    return Pipeline
+      .builder()
+      .withApplication(getString(config, "application"))
+      .withName(getString(config, "name"))
+      .withPipelineConfigId(getString(config, "id"))
+      .withTrigger((Map<String, Object>) config.get("trigger"))
+      .withStages((List<Map<String, Object>>) config.get("stages"))
+      .withAppConfig((Map<String, Serializable>) config.get("appConfig"))
+      .withParallel(getBoolean(config, "parallel"))
+      .withLimitConcurrent(getBoolean(config, "limitConcurrent"))
+      .withKeepWaitingPipelines(getBoolean(config, "keepWaitingPipelines"))
+      .withExecutingInstance(currentInstance)
+      .withNotifications((List<Map<String, Object>>) config.get("notifications"))
+      .build();
+  }
+
+  private boolean getBoolean(Map<String, ?> map, String key) {
+    return parseBoolean(getString(map, key));
+  }
+
+  private String getString(Map<String, ?> map, String key) {
+    return map.containsKey(key) ? map.get(key).toString() : null;
+  }
+
+  @Override protected boolean shouldQueue(Pipeline execution) {
+    return startTracker == null ||
+      execution.getPipelineConfigId() == null ||
+      startTracker.queueIfNotStarted(execution.getPipelineConfigId(), execution.getId());
+  }
+}
---
+++ orca-core/src/test/groovy/com/netflix/spinnaker/orca/pipeline/ExecutionLauncherSpec.groovy
@@ -0,0 +1,182 @@
+/*
+ * Copyright 2016 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netflix.spinnaker.orca.pipeline
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.netflix.appinfo.InstanceInfo
+import com.netflix.spinnaker.orca.Task
+import com.netflix.spinnaker.orca.pipeline.model.Execution
+import com.netflix.spinnaker.orca.pipeline.model.Pipeline
+import org.springframework.context.annotation.AnnotationConfigApplicationContext
+import spock.lang.Shared
+import spock.lang.Specification
+import spock.lang.Subject
+import spock.util.concurrent.BlockingVariable
+import static com.netflix.spinnaker.orca.jackson.OrcaJackson.TYPE_IDENTIFIER
+import static com.netflix.spinnaker.orca.pipeline.ExecutionLauncher.ExecutionRunner
+import static com.netflix.spinnaker.orca.pipeline.StageDefinitionBuilder.TaskDefinition
+
+abstract class ExecutionLauncherSpec<T extends Execution, L extends ExecutionLauncher<T>> extends Specification {
+
+  abstract L create(StageDefinitionBuilder... stageDefBuilders)
+
+  @Shared def objectMapper = new ObjectMapper()
+  @Shared def instanceInfo = InstanceInfo.Builder.newBuilder().setAppName("orca").setHostName("localhost").build()
+  def runner = Mock(ExecutionRunner)
+
+  def "builds tasks for each stage"() {
+    given:
+    def stageDefBuilder = Stub(StageDefinitionBuilder) {
+      getType() >> stageType
+      taskGraph() >> [new TaskDefinition("1", "1", Task)]
+    }
+    @Subject def launcher = create(stageDefBuilder)
+
+    and:
+    def pipeline = new BlockingVariable<Pipeline>()
+    runner.start(_) >> { Pipeline p -> pipeline.set(p) }
+
+    when:
+    launcher.start(json)
+
+    then:
+    with(pipeline.get().stages) {
+      tasks.id.flatten() == ["1"]
+    }
+
+    where:
+    stageType = "foo"
+    config = [
+      id    : "whatever",
+      stages: [
+        [(TYPE_IDENTIFIER): "PipelineStage", type: stageType]
+      ]
+    ]
+    json = objectMapper.writeValueAsString(config)
+  }
+}
+
+class PipelineLauncherSpec extends ExecutionLauncherSpec<Pipeline, PipelineLauncher> {
+
+  def startTracker = Stub(PipelineStartTracker)
+
+  @Override
+  PipelineLauncher create(StageDefinitionBuilder... stageDefBuilders) {
+    return new PipelineLauncher(objectMapper, instanceInfo, runner, stageDefBuilders.toList(), startTracker)
+  }
+
+  def "can autowire pipeline launcher with optional dependencies"() {
+    given:
+    def context = new AnnotationConfigApplicationContext()
+    context.with {
+      beanFactory.with {
+        registerSingleton("objectMapper", objectMapper)
+        registerSingleton("executionRunner", runner)
+        registerSingleton("instanceInfo", instanceInfo)
+        registerSingleton("whateverStageDefBuilder", new StageDefinitionBuilderSupport() {
+          @Override
+          String getType() {
+            return "whatever"
+          }
+        })
+        registerSingleton("pipelineStartTracker", startTracker)
+      }
+      register(PipelineLauncher)
+      refresh()
+    }
+
+    expect:
+    context.getBean(PipelineLauncher)
+  }
+
+  def "can autowire pipeline launcher without optional dependencies"() {
+    given:
+    def context = new AnnotationConfigApplicationContext()
+    context.with {
+      beanFactory.with {
+        registerSingleton("objectMapper", objectMapper)
+        registerSingleton("executionRunner", runner)
+        registerSingleton("instanceInfo", instanceInfo)
+        registerSingleton("whateverStageDefBuilder", new StageDefinitionBuilderSupport() {
+          @Override
+          String getType() {
+            return "whatever"
+          }
+        })
+      }
+      register(PipelineLauncher)
+      refresh()
+    }
+
+    expect:
+    context.getBean(PipelineLauncher)
+  }
+
+  def "does not start pipeline if it should be queued"() {
+    given:
+    startTracker.queueIfNotStarted(*_) >> true
+
+    and:
+    @Subject def launcher = create()
+
+    when:
+    launcher.start(json)
+
+    then:
+    0 * runner.start(_)
+
+    where:
+    config = [pipelineConfigId: "whatever", stages: []]
+    json = objectMapper.writeValueAsString(config)
+  }
+
+  def "does not start pipeline if it does not have a pipeline config id"() {
+    given:
+    startTracker.queueIfNotStarted(*_) >> false
+
+    and:
+    @Subject def launcher = create()
+
+    when:
+    launcher.start(json)
+
+    then:
+    0 * runner.start(_)
+
+    where:
+    config = [stages: []]
+    json = objectMapper.writeValueAsString(config)
+  }
+
+  def "starts pipeline if it should not be queued"() {
+    given:
+    startTracker.queueIfNotStarted(*_) >> false
+
+    and:
+    @Subject def launcher = create()
+
+    when:
+    launcher.start(json)
+
+    then:
+    1 * runner.start(_)
+
+    where:
+    config = [id: "whatever", stages: []]
+    json = objectMapper.writeValueAsString(config)
+  }
+}
