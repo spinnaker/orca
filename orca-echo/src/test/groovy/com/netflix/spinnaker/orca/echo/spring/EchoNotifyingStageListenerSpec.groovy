1ce22167927149449e7802cd09caebf4291b1830
Rob Fletcher <rfletcher@netflix.com>
2016-09-09 13:49:59 -0700

    fix echo listener for v2 execution engine

--- orca-echo/src/test/groovy/com/netflix/spinnaker/orca/echo/spring/EchoNotifyingStageListenerSpec.groovy
+++ orca-echo/src/test/groovy/com/netflix/spinnaker/orca/echo/spring/EchoNotifyingStageListenerSpec.groovy
@@ -7,8 +7,7 @@ import spock.lang.Shared
 import spock.lang.Specification
 import spock.lang.Subject
 import spock.lang.Unroll
-import static org.hamcrest.Matchers.containsInAnyOrder
-import static spock.util.matcher.HamcrestSupport.expect
+import static com.netflix.spinnaker.orca.ExecutionStatus.*
 
 class EchoNotifyingStageListenerSpec extends Specification {
 
@@ -31,23 +30,18 @@ class EchoNotifyingStageListenerSpec extends Specification {
     echoListener.beforeTask(null, pipelineStage, task)
 
     then:
-    1 * echoService.recordEvent(_)
+    1 * echoService.recordEvent({ event -> event.details.type == "orca:task:starting" })
   }
 
   def "triggers an event when a stage starts"() {
     given:
     def task = new DefaultTask(stageStart: true)
 
-    and:
-    def events = []
-    echoService.recordEvent(_) >> { events << it[0]; null }
-
     when:
-    echoListener.beforeTask(null, pipelineStage, task)
+    echoListener.beforeStage(null, pipelineStage)
 
     then:
-    events.size() == 2
-    expect events.details.type, containsInAnyOrder("orca:task:starting", "orca:stage:starting")
+    1 * echoService.recordEvent({ event -> event.details.type == "orca:stage:starting" })
   }
 
   def "triggers an event when a task starts"() {
@@ -78,14 +72,28 @@ class EchoNotifyingStageListenerSpec extends Specification {
     invocations * echoService.recordEvent(_)
 
     where:
-    invocations | stage              | taskName   | executionStatus           | wasSuccessful | isEnd
-    0           | orchestrationStage | "stageEnd" | ExecutionStatus.RUNNING   | true          | false
-    1           | orchestrationStage | "stageEnd" | ExecutionStatus.STOPPED   | true          | false
-    1           | pipelineStage      | "xxx"      | ExecutionStatus.SUCCEEDED | true          | false
-    2           | pipelineStage      | "stageEnd" | ExecutionStatus.SUCCEEDED | true          | false
-    2           | pipelineStage      | "stageEnd" | ExecutionStatus.SUCCEEDED | false         | false
-    2           | pipelineStage      | "xxx"      | ExecutionStatus.SUCCEEDED | true          | true
-    1           | orchestrationStage | "xxx"      | ExecutionStatus.SUCCEEDED | true          | true
+    invocations | stage              | taskName   | executionStatus | wasSuccessful | isEnd
+    0           | orchestrationStage | "stageEnd" | RUNNING         | true          | false
+    1           | orchestrationStage | "stageEnd" | STOPPED         | true          | false
+    1           | pipelineStage      | "xxx"      | SUCCEEDED       | true          | false
+    2           | pipelineStage      | "stageEnd" | SUCCEEDED       | true          | false
+    2           | pipelineStage      | "stageEnd" | SUCCEEDED       | false         | false
+    1           | pipelineStage      | "xxx"      | SUCCEEDED       | true          | true // is end but v2 version so not triggered by afterTask
+    1           | orchestrationStage | "xxx"      | SUCCEEDED       | true          | true
+  }
+
+  @Unroll
+  def "triggers an end stage event"() {
+    when:
+    echoListener.afterStage(null, stage)
+
+    then:
+    invocations * echoService.recordEvent(_)
+
+    where:
+    invocations | stage
+    1           | pipelineStage
+    0           | orchestrationStage
   }
 
   @Unroll
@@ -112,10 +120,10 @@ class EchoNotifyingStageListenerSpec extends Specification {
     message.content.taskName == taskName
 
     where:
-    stage              | taskName   | executionStatus           | wasSuccessful || echoMessage || type    || standalone
-    orchestrationStage | "xxx"      | ExecutionStatus.STOPPED   | true          || "complete"  || "task"  || true
-    pipelineStage      | "xxx"      | ExecutionStatus.SUCCEEDED | true          || "complete"  || "task"  || false
-    pipelineStage      | "stageEnd" | ExecutionStatus.SUCCEEDED | true          || "complete"  || "stage" || false
-    pipelineStage      | "stageEnd" | ExecutionStatus.SUCCEEDED | false         || "failed"    || "stage" || false
+    stage              | taskName   | executionStatus | wasSuccessful || echoMessage || type    || standalone
+    orchestrationStage | "xxx"      | STOPPED         | true          || "complete"  || "task"  || true
+    pipelineStage      | "xxx"      | SUCCEEDED       | true          || "complete"  || "task"  || false
+    pipelineStage      | "stageEnd" | SUCCEEDED       | true          || "complete"  || "stage" || false
+    pipelineStage      | "stageEnd" | SUCCEEDED       | false         || "failed"    || "stage" || false
   }
 }
