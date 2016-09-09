6501376b83148847cd1780fb4f48ac19aab95a5b
Rob Fletcher <rfletcher@netflix.com>
2016-09-09 13:49:59 -0700

    fix echo listener for v2 execution engine

--- orca-echo/src/main/groovy/com/netflix/spinnaker/orca/echo/spring/EchoNotifyingStageListener.groovy
+++ orca-echo/src/main/groovy/com/netflix/spinnaker/orca/echo/spring/EchoNotifyingStageListener.groovy
@@ -6,11 +6,10 @@ import com.netflix.spinnaker.orca.ExecutionStatus
 import com.netflix.spinnaker.orca.echo.EchoService
 import com.netflix.spinnaker.orca.listeners.Persister
 import com.netflix.spinnaker.orca.listeners.StageListener
-import com.netflix.spinnaker.orca.pipeline.model.Orchestration
-import com.netflix.spinnaker.orca.pipeline.model.Pipeline
-import com.netflix.spinnaker.orca.pipeline.model.Stage
-import com.netflix.spinnaker.orca.pipeline.model.Task
+import com.netflix.spinnaker.orca.pipeline.model.*
 import org.springframework.beans.factory.annotation.Autowired
+import static com.netflix.spinnaker.orca.ExecutionStatus.NOT_STARTED
+import static com.netflix.spinnaker.orca.ExecutionStatus.RUNNING
 
 /**
  * Converts execution events to Echo events.
@@ -27,28 +26,39 @@ class EchoNotifyingStageListener implements StageListener {
   }
 
   @Override
-  void beforeTask(Persister persister, Stage stage, Task task) {
-    if (task.status == ExecutionStatus.NOT_STARTED) {
+  <T extends Execution<T>> void beforeTask(Persister persister,
+                                           Stage<T> stage,
+                                           Task task) {
+    if (task.status == NOT_STARTED) {
       recordEvent('task', 'starting', stage, task)
-      if (task.stageStart) {
-        recordEvent("stage", "starting", stage, task)
-      }
     }
   }
 
   @Override
-  void afterTask(Persister persister, Stage stage, Task task, ExecutionStatus executionStatus, boolean wasSuccessful) {
-    if (executionStatus == ExecutionStatus.RUNNING) {
+  <T extends Execution<T>> void beforeStage(Persister persister,
+                                            Stage<T> stage) {
+    if (stage.status == NOT_STARTED) {
+      recordEvent("stage", "starting", stage)
+    }
+  }
+
+  @Override
+  <T extends Execution<T>> void afterTask(Persister persister,
+                                          Stage<T> stage,
+                                          Task task,
+                                          ExecutionStatus executionStatus,
+                                          boolean wasSuccessful) {
+    if (executionStatus == RUNNING) {
       return
     }
 
     recordEvent('task', (wasSuccessful ? "complete" : "failed"), stage, task)
+
+    // TODO: this should all be deleted once we move to v2 engine
     if (stage.execution instanceof Pipeline) {
       if (wasSuccessful) {
-        // TODO: latter part of condition is deprecated
-        if (task.stageEnd || task.name.contains('stageEnd')) {
+        if (task.name.contains('stageEnd')) {
           recordEvent('stage', 'complete', stage, task)
-          // TODO: deprecated
         } else if (task.name.contains('stageStart')) {
           recordEvent('stage', 'starting', stage, task)
         }
@@ -58,9 +68,29 @@ class EchoNotifyingStageListener implements StageListener {
     }
   }
 
+  @Override
+  <T extends Execution<T>> void afterStage(Persister persister,
+                                           Stage<T> stage) {
+    if (stage.execution instanceof Pipeline) {
+      if (stage.status.successful) {
+        recordEvent('stage', 'complete', stage)
+      } else {
+        recordEvent('stage', 'failed', stage)
+      }
+    }
+  }
+
   private void recordEvent(String type, String phase, Stage stage, Task task) {
+    recordEvent(type, phase, stage, Optional.of(task))
+  }
+
+  private void recordEvent(String type, String phase, Stage stage) {
+    recordEvent(type, phase, stage, Optional.empty())
+  }
+
+  private void recordEvent(String type, String phase, Stage stage, Optional<Task> maybeTask) {
     try {
-      echoService.recordEvent(
+      def event = [
         details: [
           source     : "orca",
           type       : "orca:${type}:${phase}".toString(),
@@ -70,15 +100,18 @@ class EchoNotifyingStageListener implements StageListener {
           standalone : stage.execution instanceof Orchestration,
           canceled   : stage.execution.canceled,
           context    : stage.context,
-          taskName   : task.name,
           startTime  : stage.startTime,
           endTime    : stage.endTime,
           execution  : stage.execution,
           executionId: stage.execution.id
         ]
-      )
+      ]
+      maybeTask.ifPresent { Task task ->
+        event.content.taskName = task.name
+      }
+      echoService.recordEvent(event)
     } catch (Exception e) {
-      log.error("Failed to send ${type} event ${phase} ${stage.execution.id} ${task.name}", e)
+      log.error("Failed to send ${type} event ${phase} ${stage.execution.id} ${maybeTask.map { Task task -> task.name }}", e)
     }
   }
 
