7d619fab0ea64509b9e2dcba38b8e40cb1f2c5e1
andrewbackes <rbackes@ucla.edu>
2017-10-12 18:18:38 -0700

    Make variable name clearly describe its purpose

--- orca-clouddriver/src/main/groovy/com/netflix/spinnaker/orca/clouddriver/utils/MonikerHelper.java
+++ orca-clouddriver/src/main/groovy/com/netflix/spinnaker/orca/clouddriver/utils/MonikerHelper.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2017 Armory, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.netflix.spinnaker.orca.clouddriver.utils;
 
 
@@ -12,8 +27,8 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class MonikerHelper {
-  public String getAppNameFromStage(Stage stage, String fallbackServerGroupName) {
-    Names names = Names.parseName(fallbackServerGroupName);
+  public String getAppNameFromStage(Stage stage, String fallbackFriggaName) {
+    Names names = Names.parseName(fallbackFriggaName);
     Moniker moniker = (Moniker) stage.getContext().get("moniker");
     String appName;
     if (moniker != null && moniker.getApp() != null) {
@@ -24,8 +39,8 @@ public class MonikerHelper {
     return appName;
   }
 
-  public String getClusterNameFromStage(Stage stage, String fallbackServerGroupName) {
-    Names names = Names.parseName(fallbackServerGroupName);
+  public String getClusterNameFromStage(Stage stage, String fallbackFriggaName) {
+    Names names = Names.parseName(fallbackFriggaName);
     Moniker moniker = (Moniker) stage.getContext().get("moniker");
     String clusterName;
     if (moniker != null && moniker.getCluster() != null) {
