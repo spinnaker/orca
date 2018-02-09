spinnaker/spinnaker

commit 2dd01d7d66342f2d09ac9d47568e977b96c3c256

fix(pipelines): Improve pipeline error reporting (#1937)
When a pipeline fails validation, due to its being disabled, having
an invalid template, or being missing an artifact, log a failed execution
to the execution history instead of only generating an error in the
logs.

orca-web/src/main/groovy/com/netflix/spinnaker/orca/controllers/OperationsController.groovy
@@ -77,15 +77,45 @@ class OperationsController {
 
   @RequestMapping(value = "/orchestrate", method = RequestMethod.POST)
   Map<String, Object> orchestrate(@RequestBody Map pipeline, HttpServletResponse response) {
-    parsePipelineTrigger(executionRepository, buildService, pipeline)
-    Trigger trigger = objectMapper.convertValue(pipeline.trigger, Trigger)
-
-    boolean plan = pipeline.plan ?: false
-    for (PipelinePreprocessor preprocessor : (pipelinePreprocessors ?: [])) {
-      pipeline = preprocessor.process(pipeline)
+    Exception pipelineError = null
+    try {
+      parseAndValidatePipeline(pipeline)
+    } catch (Exception e) {
+      pipelineError = e
     }
 
-    pipeline.trigger = trigger
+    if (pipeline.plan) {
+      log.info('Not starting pipeline (plan: true): {}', value("pipelineId", pipeline.id))
+      if (pipelineError != null) {
+        throw pipelineError
+      }
+      return pipeline
+    }
+
+    def augmentedContext = [trigger: pipeline.trigger]
+    def processedPipeline = contextParameterProcessor.process(pipeline, augmentedContext, false)
+
+    if (pipelineError == null) {
+      startPipeline(processedPipeline)
+    } else {
+      markPipelineFailed(processedPipeline, pipelineError)
+      throw pipelineError
+    }
+  }
+
+  private void parseAndValidatePipeline(Map pipeline) {
+    try {
+      parsePipelineTrigger(executionRepository, buildService, pipeline)
+
+      for (PipelinePreprocessor preprocessor : (pipelinePreprocessors ?: [])) {
+        pipeline = preprocessor.process(pipeline)
+      }
+    } finally {
+      // Even if an exception is thrown above, we need to convert pipeline.trigger to
+      // a Trigger object, as the code to log the error to the execution history is
+      // expecting this.
+      pipeline.trigger = objectMapper.convertValue(pipeline.trigger, Trigger)
+    }
 
     def json = objectMapper.writeValueAsString(pipeline)
     log.info('received pipeline {}:{}', value("pipelineId", pipeline.id), json)
@@ -99,22 +129,9 @@ class OperationsController {
       convertLinearToParallel(pipeline)
     }
 
-    if (plan) {
-      log.info('not starting pipeline (plan: true): {}', value("pipelineId", pipeline.id))
-      if (pipeline.errors != null) {
-        throw new ValidationException("Pipeline template is invalid", pipeline.errors as List<Map<String, Object>>)
-      }
-      return pipeline
-    }
-
-    def augmentedContext = [trigger: pipeline.trigger]
-    def processedPipeline = contextParameterProcessor.process(pipeline, augmentedContext, false)
-
     if (pipeline.errors != null) {
       throw new ValidationException("Pipeline template is invalid", pipeline.errors as List<Map<String, Object>>)
     }
-
-    startPipeline(processedPipeline)
   }
 
   private void parsePipelineTrigger(ExecutionRepository executionRepository, BuildService buildService, Map pipeline) {
@@ -247,6 +264,16 @@ class OperationsController {
     log.info('requested pipeline: {}', json)
 
     def pipeline = executionLauncher.start(PIPELINE, json)
+
+    [ref: "/pipelines/${pipeline.id}".toString()]
+  }
+
+  private Map<String, String> markPipelineFailed(Map config, Exception e) {
+    injectPipelineOrigin(config)
+    def json = objectMapper.writeValueAsString(config)
+    log.info('requested pipeline: {}', json)
+
+    def pipeline = executionLauncher.fail(PIPELINE, json, e)
 
     [ref: "/pipelines/${pipeline.id}".toString()]
   }

orca-core/src/main/java/com/netflix/spinnaker/orca/pipeline/ExecutionLauncher.java
@@ -92,6 +92,22 @@ public class ExecutionLauncher {
     return execution;
   }
 
+  /**
+   * Log that an execution failed; useful if a pipeline failed validation and we want to persist the
+   * failure to the execution history but don't actually want to attempt to run the execution.
+   *
+   * @param e the exception that was thrown during pipeline validation
+   */
+  public Execution fail(ExecutionType type, String configJson, Exception e) throws Exception {
+    final Execution execution = parse(type, configJson);
+
+    persistExecution(execution);
+
+    handleStartupFailure(execution, e);
+
+    return execution;
+  }
+
   private void checkRunnable(Execution execution) {
     if (execution.getType() == PIPELINE) {
       pipelineValidator.ifPresent(it -> it.checkRunnable(execution));

orca-web/src/test/groovy/com/netflix/spinnaker/orca/controllers/OperationsControllerSpec.groovy
@@ -18,6 +18,7 @@ package com.netflix.spinnaker.orca.controllers
 
 import javax.servlet.http.HttpServletResponse
 import com.netflix.spinnaker.kork.web.exceptions.InvalidRequestException
+import com.netflix.spinnaker.kork.web.exceptions.ValidationException
 import com.netflix.spinnaker.orca.igor.BuildArtifactFilter
 import com.netflix.spinnaker.orca.igor.BuildService
 import com.netflix.spinnaker.orca.jackson.OrcaObjectMapper
@@ -46,6 +47,7 @@ import static com.netflix.spinnaker.orca.ExecutionStatus.SUCCEEDED
 import static com.netflix.spinnaker.orca.pipeline.model.Execution.ExecutionType
 import static com.netflix.spinnaker.orca.pipeline.model.Execution.ExecutionType.PIPELINE
 import static com.netflix.spinnaker.orca.test.model.ExecutionBuilder.pipeline
+import static java.lang.String.format
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 
 class OperationsControllerSpec extends Specification {
@@ -60,7 +62,7 @@ class OperationsControllerSpec extends Specification {
   def executionRepository = Mock(ExecutionRepository)
   def pipelineTemplateService = Mock(PipelineTemplateService)
   def webhookService = Mock(WebhookService)
-  def artifactResolver = new ArtifactResolver(mapper, executionRepository)
+  def artifactResolver = Mock(ArtifactResolver)
 
   def env = new MockEnvironment()
   def buildArtifactFilter = new BuildArtifactFilter(environment: env)
@@ -548,6 +550,56 @@ class OperationsControllerSpec extends Specification {
       throw new ExecutionNotFoundException("Not found")
     }
     0 * executionLauncher.start(*_)
+  }
+
+  def "should log and re-throw validation error for non-templated pipeline"() {
+    given:
+    def pipelineConfig = [
+      type: PIPELINE,
+      errors: [
+        'things broke': 'because of the way it is'
+      ]
+    ]
+    def response = Mock(HttpServletResponse)
+    Execution startedPipeline = null
+    executionLauncher.fail(*_) >> { ExecutionType type, String json, Throwable t ->
+      startedPipeline = mapper.readValue(json, Execution)
+      startedPipeline.id = UUID.randomUUID().toString()
+      startedPipeline
+    }
+
+    when:
+    controller.orchestrate(pipelineConfig, response)
+
+    then:
+    thrown(ValidationException)
+    0 * executionLauncher.start(*_)
+
+  }
+
+  def "should log and re-throw missing artifact error"() {
+    given:
+    def pipelineConfig = [
+      type: PIPELINE
+    ]
+    def response = Mock(HttpServletResponse)
+    artifactResolver.resolveArtifacts(*_) >> { Map pipeline ->
+      throw new IllegalStateException(format("Unmatched expected artifact could not be resolved."))
+    }
+    Execution startedPipeline = null
+    executionLauncher.fail(*_) >> { ExecutionType type, String json, Throwable t ->
+      startedPipeline = mapper.readValue(json, Execution)
+      startedPipeline.id = UUID.randomUUID().toString()
+      startedPipeline
+    }
+
+    when:
+    controller.orchestrate(pipelineConfig, response)
+
+    then:
+    thrown(IllegalStateException)
+    0 * executionLauncher.start(*_)
+
   }
 
   def "should return empty list if webhook stage is not enabled"() {

spinnaker/orca

commit 3757a8eb7a8084a321d4ecb2815f368e1651b325

fix(pipelines): Fix templated pipelines not being triggered
This fixes a regression introduced in #1937. The preprocessors are not mutating `pipeline` inline, so we have to return the new, rendered pipeline back to caller.

orca-web/src/main/groovy/com/netflix/spinnaker/orca/controllers/OperationsController.groovy
@@ -81,7 +81,7 @@ class OperationsController {
   Map<String, Object> orchestrate(@RequestBody Map pipeline, HttpServletResponse response) {
     Exception pipelineError = null
     try {
-      parseAndValidatePipeline(pipeline)
+      pipeline = parseAndValidatePipeline(pipeline)
     } catch (Exception e) {
       pipelineError = e
     }
@@ -106,7 +106,7 @@ class OperationsController {
     }
   }
 
-  private void parseAndValidatePipeline(Map pipeline) {
+  private Map parseAndValidatePipeline(Map pipeline) {
     parsePipelineTrigger(executionRepository, buildService, pipeline)
 
     for (PipelinePreprocessor preprocessor : (pipelinePreprocessors ?: [])) {
@@ -128,6 +128,7 @@ class OperationsController {
     if (pipeline.errors != null) {
       throw new ValidationException("Pipeline template is invalid", pipeline.errors as List<Map<String, Object>>)
     }
+    return pipeline
   }
 
   private void parsePipelineTrigger(ExecutionRepository executionRepository, BuildService buildService, Map pipeline) {