11329661ca71bf0fb4a53549bababb85486cfd50
Daniel Peach <dpeach@dpeach.nyc.corp.google.com>
2018-03-13 09:33:07 -0400

    feat(provider/kubernetes): retry manifest force cache refresh on failure

--- orca-clouddriver/src/main/groovy/com/netflix/spinnaker/orca/clouddriver/tasks/manifest/ManifestForceCacheRefreshTask.java
+++ orca-clouddriver/src/main/groovy/com/netflix/spinnaker/orca/clouddriver/tasks/manifest/ManifestForceCacheRefreshTask.java
@@ -19,21 +19,33 @@ package com.netflix.spinnaker.orca.clouddriver.tasks.manifest;
 
 import com.google.common.collect.ImmutableMap;
 import com.netflix.spinnaker.orca.ExecutionStatus;
-import com.netflix.spinnaker.orca.Task;
+import com.netflix.spinnaker.orca.RetryableTask;
 import com.netflix.spinnaker.orca.TaskResult;
 import com.netflix.spinnaker.orca.clouddriver.CloudDriverCacheService;
 import com.netflix.spinnaker.orca.clouddriver.tasks.AbstractCloudProviderAwareTask;
 import com.netflix.spinnaker.orca.pipeline.model.Stage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import retrofit.RetrofitError;
 
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @Component
-public class ManifestForceCacheRefreshTask extends AbstractCloudProviderAwareTask implements Task {
+public class ManifestForceCacheRefreshTask extends AbstractCloudProviderAwareTask implements RetryableTask {
   private final static String REFRESH_TYPE = "manifest";
   public final static String TASK_NAME = "forceCacheRefresh";
+
+  @Override
+  public long getBackoffPeriod() {
+    return TimeUnit.SECONDS.toMillis(10);
+  }
+
+  @Override
+  public long getTimeout() {
+    return TimeUnit.MINUTES.toMillis(15);
+  }
 
   @Autowired
   CloudDriverCacheService cacheService;
@@ -44,21 +56,25 @@ public class ManifestForceCacheRefreshTask extends AbstractCloudProviderAwareTas
     String account = getCredentials(stage);
     Map<String, List<String>> deployedManifests = (Map<String, List<String>>) stage.getContext().get("outputs.manifestNamesByNamespace");
 
+    ExecutionStatus status = ExecutionStatus.SUCCEEDED;
     for (Map.Entry<String, List<String>> entry : deployedManifests.entrySet()) {
       String location = entry.getKey();
-      entry.getValue().forEach(name -> {
+      for (String name : entry.getValue()) {
         Map<String, String> request = new ImmutableMap.Builder<String, String>()
-            .put("account", account)
-            .put("name", name)
-            .put("location", location)
-            .build();
+          .put("account", account)
+          .put("name", name)
+          .put("location", location)
+          .build();
 
-        cacheService.forceCacheUpdate(cloudProvider, REFRESH_TYPE, request);
-      });
-
-      // TODO(lwander): make sure cache refresh is processed
+        try {
+          // TODO(lwander): make sure cache refresh is processed.
+          cacheService.forceCacheUpdate(cloudProvider, REFRESH_TYPE, request);
+        } catch (RetrofitError re) {
+          status = ExecutionStatus.RUNNING;
+        }
+      }
     }
 
-    return new TaskResult(ExecutionStatus.SUCCEEDED);
+    return new TaskResult(status);
   }
 }
