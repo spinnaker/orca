5faa8ef142e2b2094f27b250d3faf88dd37ac733
Emily Burns <emjeburns@gmail.com>
2018-08-21 17:16:30 -0700

    fix(metrics): null value is string null

--- orca-front50/src/main/groovy/com/netflix/spinnaker/orca/front50/DependentPipelineStarter.groovy
+++ orca-front50/src/main/groovy/com/netflix/spinnaker/orca/front50/DependentPipelineStarter.groovy
@@ -143,7 +143,7 @@ class DependentPipelineStarter implements ApplicationContextAware {
         pipeline = executionLauncher().start(PIPELINE, json)
 
         Id id = registry.createId("pipelines.triggered")
-          .withTag("application", Optional.ofNullable(pipeline.getApplication()).orElse("NULL_APPLICATION"))
+          .withTag("application", Optional.ofNullable(pipeline.getApplication()).orElse("null"))
           .withTag("monitor", getClass().getSimpleName())
         registry.counter(id).increment()
 
