326c705c665abb1f65976119c5dc28fefb6eaafa
Matt Brown <mattbrown@spotify.com>
2019-06-18 10:21:11 -0400

    perf(artifacts): use pageSize=1 when resolving prior artifacts (#2955) (#2991)
    This is a smaller-scoped attempt at
    https://github.com/spinnaker/orca/pull/2938, which relates to
    https://github.com/spinnaker/spinnaker/issues/4367.
    
    `ArtifactResolver.resolveArtifacts(Map)` currently attempts to load
    _all_ executions for a pipeline in order to find the most recent
    execution in the course of resolving expected artifacts. This causes a
    lot of unnecessary data to be loaded from Redis/SQL, only to be
    discarded a few operations later.
    
    This change makes use of the fact that the ExecutionRepository
    implementations will respect the `executionCriteria.pageSize` argument
    when retrieving Execitions for a pipelineId.
    
    In the Redis-based implementation, the executions are stored in a sorted
    set scored on `buildTime` (or `currentTimeMillis()` if `buildTime` is
    null), so retrieving all of the executions for the pipelineId with a
    pageSize=1 should load the Execution with the most recent `buildTime`.
    
    In the SQL-based implementation,
    `retrievePipelinesForPipelineConfigId(String, ExecutionCriteria)` sorts
    the query results based on the `id` field.
    
    For both implementations, this is a small change from the existing
    behavior of ArtifactResolver, which retrieves all executions and then
    uses the one with the most recent `startTime` (or `id`). This change
    seems like it will lead to the same result in most cases though, since
    buildTime is set when the Execution is stored, and the `id` field is
    ascending based on the timestamp of when it is generated.
    
    The `retrievePipelinesForPipelienConfigId` method in both
    implementations currently ignores the `executionCriteria.sortType`
    field, but I've added this in the call from ArtifactResolver to at
    least document ArtifactResolver's desire.

--- orca-core/src/main/java/com/netflix/spinnaker/orca/pipeline/util/ArtifactResolver.java
+++ orca-core/src/main/java/com/netflix/spinnaker/orca/pipeline/util/ArtifactResolver.java
@@ -175,7 +175,7 @@ public class ArtifactResolver {
       return;
     }
 
-    List<Artifact> priorArtifacts = getArtifactsForPipelineId((String) pipeline.get("id"), new ExecutionCriteria());
+    List<Artifact> priorArtifacts = getPriorArtifacts(pipeline);
     Set<Artifact> resolvedArtifacts = resolveExpectedArtifacts(expectedArtifacts, receivedArtifacts, priorArtifacts, true);
     Set<Artifact> allArtifacts = new HashSet<>(receivedArtifacts);
 
@@ -187,6 +187,16 @@ public class ArtifactResolver {
     } catch (IOException e) {
       throw new ArtifactResolutionException("Failed to store artifacts in trigger: " + e.getMessage(), e);
     }
+  }
+
+  private List<Artifact> getPriorArtifacts(final Map<String, Object> pipeline) {
+    // set pageSize to a single record to avoid hydrating all of the stored Executions for
+    // the pipeline, since getArtifactsForPipelineId only uses the most recent Execution from the
+    // returned Observable<Execution>
+    ExecutionCriteria criteria = new ExecutionCriteria();
+    criteria.setPageSize(1);
+    criteria.setSortType(ExecutionRepository.ExecutionComparator.START_TIME_OR_ID);
+    return getArtifactsForPipelineId((String) pipeline.get("id"), criteria);
   }
 
   public Artifact resolveSingleArtifact(ExpectedArtifact expectedArtifact, List<Artifact> possibleMatches, boolean requireUniqueMatches) {
