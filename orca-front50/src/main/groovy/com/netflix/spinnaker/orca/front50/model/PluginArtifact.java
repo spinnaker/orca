6d42480da3b30a4cd943849bf2c7d07826fae65e
chris smalley <csmalley@netflix.com>
2020-01-08 09:59:09 -0800

    feat(plugins): Add upsert and delete plugin artifact tasks

---
+++ orca-front50/src/main/groovy/com/netflix/spinnaker/orca/front50/model/PluginArtifact.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2020 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netflix.spinnaker.orca.front50.model;
+
+import java.util.List;
+import java.util.regex.Pattern;
+import javax.annotation.Nonnull;
+import lombok.Data;
+
+@Data
+public class PluginArtifact {
+  @Nonnull private String id;
+  private String description;
+  private String provider;
+  @Nonnull private List<Release> releases;
+  private Long createTs;
+  private Long lastModified;
+  private String lastModifiedBy;
+
+  @Data
+  public static class Release {
+    public static final Pattern SUPPORTS_PATTERN =
+        Pattern.compile(
+            "^(?<service>[\\w\\-]+)(?<operator>[><=]{1,2})(?<version>[0-9]+\\.[0-9]+\\.[0-9]+)$");
+    public static final String SUPPORTS_PATTERN_SERVICE_GROUP = "service";
+    public static final String SUPPORTS_PATTERN_OPERATOR_GROUP = "operator";
+    public static final String SUPPORTS_PATTERN_VERSION_GROUP = "version";
+
+    private String version;
+    private String date;
+
+    private List<String> requires;
+    private String url;
+    private String sha512sum;
+    private State state;
+    private String lastModifiedBy;
+
+    public enum State {
+      CANDIDATE,
+      RELEASE
+    }
+  }
+}
