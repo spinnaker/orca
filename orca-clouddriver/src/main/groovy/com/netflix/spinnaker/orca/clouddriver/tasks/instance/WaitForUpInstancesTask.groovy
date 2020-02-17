spinnaker/spinnaker

commit 2801fb98015b68d2261fa1d9bd68824fbbe7edf7

fix(SPIN-5372): changing ceiling calculation to round for healthyDeployPercentage (#3441)

orca-clouddriver/src/main/groovy/com/netflix/spinnaker/orca/clouddriver/tasks/instance/WaitForUpInstancesTask.groovy
@@ -141,7 +141,7 @@ class WaitForUpInstancesTask extends AbstractWaitingForInstancesTask {
         throw new NumberFormatException("targetHealthyDeployPercentage must be an integer between 0 and 100")
       }
 
-      def newTargetDesiredSize = Math.ceil(percentage * targetDesiredSize / 100D) as Integer
+      def newTargetDesiredSize = Math.max(Math.round(percentage * targetDesiredSize / 100D), 1) as Integer
       splainer.add("setting targetDesiredSize=${newTargetDesiredSize} based on configured targetHealthyDeployPercentage=${percentage}% of previous targetDesiredSize=${targetDesiredSize}")
       targetDesiredSize = newTargetDesiredSize
     } else if (stage.context.desiredPercentage != null) {

orca-clouddriver/src/test/groovy/com/netflix/spinnaker/orca/clouddriver/tasks/instance/WaitForUpInstancesTaskSpec.groovy
@@ -177,11 +177,13 @@ class WaitForUpInstancesTaskSpec extends Specification {
     89      | 9       | 10    || true
     90      | 9       | 10    || true
     90      | 8       | 10    || false
-    91      | 9       | 10    || false
+    91      | 9       | 10    || true
+    95      | 9       | 10    || false
 
-    // verify ceiling
+    // verify round
     90      | 10      | 11    || true
-    90      | 8       | 9     || false
+    90      | 8       | 9     || true
+    95      | 8       | 9     || false
 
   }
 

spinnaker/orca

commit 04bbd85a3381b2d69c0ddb3c2519030ac324eec0

fix(wait for up): Fix to allow waiting for 0 instances
This is a regression from #3441.
It prevents waiting for 0 instances which is a valid scenario in rolling style deploys.
This should've been caught by a unittest, but the unit tests had a bug which hid this issue (fixed now)

orca-clouddriver/src/main/groovy/com/netflix/spinnaker/orca/clouddriver/tasks/instance/WaitForUpInstancesTask.groovy
@@ -141,7 +141,12 @@ class WaitForUpInstancesTask extends AbstractWaitingForInstancesTask {
         throw new NumberFormatException("targetHealthyDeployPercentage must be an integer between 0 and 100")
       }
 
-      def newTargetDesiredSize = Math.max(Math.round(percentage * targetDesiredSize / 100D), 1) as Integer
+      def newTargetDesiredSize = Math.round(percentage * targetDesiredSize / 100D) as Integer
+      if ((newTargetDesiredSize == 0) && (percentage != 0) && (targetDesiredSize > 0)) {
+        // Unless the user specified they want 0% or we actually have 0 instances in the server group,
+        // never allow 0 instances due to rounding
+        newTargetDesiredSize = 1
+      }
       splainer.add("setting targetDesiredSize=${newTargetDesiredSize} based on configured targetHealthyDeployPercentage=${percentage}% of previous targetDesiredSize=${targetDesiredSize}")
       targetDesiredSize = newTargetDesiredSize
     } else if (stage.context.desiredPercentage != null) {

orca-clouddriver/src/test/groovy/com/netflix/spinnaker/orca/clouddriver/tasks/instance/WaitForUpInstancesTaskSpec.groovy
@@ -143,7 +143,7 @@ class WaitForUpInstancesTaskSpec extends Specification {
   void 'should return #hasSucceeded for hasSucceeded when targetHealthyDeployPercentage is #percent and #healthy/#total instances are healthy'() {
     expect:
     def instances = []
-    (1..healthy).each {
+    healthy.times {
       instances << [health: [[state: 'Up']]]
     }
     def serverGroup = [
@@ -172,6 +172,7 @@ class WaitForUpInstancesTaskSpec extends Specification {
     // zero percent (should always return true)
     0       | 1       | 2     || true
     0       | 0       | 100   || true
+    100     | 0       | 0     || true
 
     // >= checks
     89      | 9       | 10    || true
@@ -184,14 +185,13 @@ class WaitForUpInstancesTaskSpec extends Specification {
     90      | 10      | 11    || true
     90      | 8       | 9     || true
     95      | 8       | 9     || false
-
   }
 
   @Unroll
   void 'should return #hasSucceeded for hasSucceeded when desiredPercentage is #percent and #healthy/#total instances are healthy'() {
     expect:
     def instances = []
-    (1..healthy).each {
+    healthy.times {
       instances << [health: [[state: 'Up']]]
     }
     def serverGroup = [
@@ -229,7 +229,7 @@ class WaitForUpInstancesTaskSpec extends Specification {
     90      | 8       | 10    | 10  | 10  || false
     91      | 9       | 10    | 10  | 10  || false
 
-    // verify ceiling
+    // verify round
     90      | 10      | 11    | 10  | 11  || true
     90      | 8       | 9     | 9   | 9   || false
 
@@ -312,7 +312,7 @@ class WaitForUpInstancesTaskSpec extends Specification {
     }
 
     def instances = []
-    (1..healthy).each {
+    healthy.times {
       instances << [health: [[state: 'Up']]]
     }
 
@@ -365,7 +365,7 @@ class WaitForUpInstancesTaskSpec extends Specification {
     }
 
     def instances = []
-    (1..healthy).each {
+    healthy.times {
       instances << [health: [[state: 'Up']]]
     }
 