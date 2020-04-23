87ca6c4479c07d261ecb3f2bd689e453c8fe9db4
edgarulg <edgar.garcia@armory.io>
2020-04-23 14:28:39 -0500

    test(bake): add test for autoDetectRegion field with true value

--- orca-bakery/src/test/groovy/com/netflix/spinnaker/orca/bakery/pipeline/BakeStageSpec.groovy
+++ orca-bakery/src/test/groovy/com/netflix/spinnaker/orca/bakery/pipeline/BakeStageSpec.groovy
@@ -89,6 +89,8 @@ class BakeStageSpec extends Specification {
     [cloudProviderType: "gce", region: "global", autoDetectRegions: false]                                | deployAz("gce", "clusters", "us-west1")                                            || expectedContexts("gce", "19700101011512", "global")
     [cloudProviderType: "aws", region: "us-west-2", autoDetectRegions: false]                             | deployAz("aws", "cluster", "us-west-1") + deployAz("aws", "cluster", "us-west-2")  || expectedContexts("aws", "19700101011512", "us-west-2")
     [cloudProviderType: "aws", regions: ["us-east-1", "us-west-1"], autoDetectRegions: false]             | deployAz("aws", "clusters", "eu-central-1")                                        || expectedContexts("aws", "19700101011512", "us-east-1", "us-west-1")
+    [cloudProviderType: "aws", regions: ["us-east-1", "us-west-1"], autoDetectRegions: true]              | deployAz("aws", "cluster", "us-west-1") + deployAz("aws", "cluster", "us-west-2")  || expectedContexts("aws", "19700101011512", "us-east-1", "us-west-1", "us-west-2")
+    [cloudProviderType: "aws", region: "us-west-2", autoDetectRegions: true]                              | deployAz("aws", "cluster", "us-west-2")                                            || expectedContexts("aws", "19700101011512", "us-west-2")
   }
 
   def "should include per-region stage contexts as global deployment details"() {
