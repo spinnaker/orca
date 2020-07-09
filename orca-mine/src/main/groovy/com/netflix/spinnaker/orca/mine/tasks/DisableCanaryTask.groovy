spinnaker/spinnaker

commit 24056301f22eb65ae2f7a425478bda09777a1736

fix(mine): Fix the wait time for mine canaries (#3758)

orca-mine/src/main/groovy/com/netflix/spinnaker/orca/mine/tasks/DisableCanaryTask.groovy
@@ -30,8 +30,6 @@ import retrofit.RetrofitError
 
 import javax.annotation.Nonnull
 
-import static com.netflix.spinnaker.orca.mine.pipeline.CanaryStage.DEFAULT_CLUSTER_DISABLE_WAIT_TIME
-
 @Component
 @Slf4j
 class DisableCanaryTask extends AbstractCloudProviderAwareTask implements Task {
@@ -42,9 +40,6 @@ class DisableCanaryTask extends AbstractCloudProviderAwareTask implements Task {
   @Nonnull
   @Override
   TaskResult execute(@Nonnull StageExecution stage) {
-
-    Integer waitTime = stage.context.clusterDisableWaitTime != null ? stage.context.clusterDisableWaitTime : DEFAULT_CLUSTER_DISABLE_WAIT_TIME
-
     try {
       def canary = mineService.getCanary(stage.context.canary.id)
       if (canary.health?.health == 'UNHEALTHY' || stage.context.unhealthy != null) {
@@ -70,8 +65,7 @@ class DisableCanaryTask extends AbstractCloudProviderAwareTask implements Task {
     return TaskResult.builder(ExecutionStatus.SUCCEEDED).context([
       'kato.last.task.id'    : taskId,
       'deploy.server.groups' : dSG,
-      disabledCluster        : selector,
-      waitTime               : waitTime
+      disabledCluster        : selector
     ]).build()
   }
 }

orca-mine/src/main/groovy/com/netflix/spinnaker/orca/mine/pipeline/CanaryDestroyClusterStage.groovy
@@ -1,0 +1,59 @@
+/*
+ * Copyright 2020 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netflix.spinnaker.orca.mine.pipeline
+
+
+import com.netflix.spinnaker.orca.api.pipeline.CancellableStage
+import com.netflix.spinnaker.orca.api.pipeline.graph.StageDefinitionBuilder
+import com.netflix.spinnaker.orca.api.pipeline.graph.TaskNode
+import com.netflix.spinnaker.orca.api.pipeline.models.StageExecution
+import com.netflix.spinnaker.orca.clouddriver.tasks.MonitorKatoTask
+import com.netflix.spinnaker.orca.clouddriver.tasks.servergroup.ServerGroupCacheForceRefreshTask
+import com.netflix.spinnaker.orca.mine.tasks.CleanupCanaryTask
+import com.netflix.spinnaker.orca.mine.tasks.CompleteCanaryTask
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+
+import javax.annotation.Nonnull
+
+@Component
+class CanaryDestroyClusterStage implements StageDefinitionBuilder, CancellableStage {
+  public static final String STAGE_TYPE = "canaryDisableCluster"
+  @Autowired CanaryStage canaryStage
+
+  @Override
+  void taskGraph(@Nonnull StageExecution stage, @Nonnull TaskNode.Builder builder) {
+    builder
+        .withTask("forceCacheRefresh", ServerGroupCacheForceRefreshTask)
+        .withTask("cleanupCanary", CleanupCanaryTask)
+        .withTask("monitorCleanup", MonitorKatoTask)
+        .withTask("completeCanary", CompleteCanaryTask)
+  }
+
+  @Override
+  Result cancel(StageExecution stage) {
+    StageExecution canaryStageInstance = stage.ancestors().find {
+      it.type == CanaryStage.PIPELINE_CONFIG_TYPE
+    }
+
+    if (!canaryStageInstance) {
+      throw new IllegalStateException("No upstream canary stage found (stageId: ${stage.id}, executionId: ${stage.execution.id})")
+    }
+
+    return canaryStage.cancel(canaryStageInstance)
+  }
+}

orca-mine/src/main/groovy/com/netflix/spinnaker/orca/mine/pipeline/CanaryDisableClusterStage.groovy
@@ -1,0 +1,79 @@
+/*
+ * Copyright 2020 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netflix.spinnaker.orca.kayenta.pipeline
+
+import com.netflix.spinnaker.orca.api.pipeline.CancellableStage
+import com.netflix.spinnaker.orca.api.pipeline.graph.StageDefinitionBuilder
+import com.netflix.spinnaker.orca.api.pipeline.graph.StageGraphBuilder
+import com.netflix.spinnaker.orca.api.pipeline.graph.TaskNode
+import com.netflix.spinnaker.orca.api.pipeline.models.StageExecution
+import com.netflix.spinnaker.orca.clouddriver.tasks.MonitorKatoTask
+import com.netflix.spinnaker.orca.mine.pipeline.CanaryDestroyClusterStage
+import com.netflix.spinnaker.orca.mine.pipeline.CanaryStage
+import com.netflix.spinnaker.orca.mine.tasks.DisableCanaryTask
+import com.netflix.spinnaker.orca.pipeline.WaitStage
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+
+import javax.annotation.Nonnull
+
+import static com.netflix.spinnaker.orca.mine.pipeline.CanaryStage.DEFAULT_CLUSTER_DISABLE_WAIT_TIME
+
+@Component
+class CanaryDisableClusterStage implements StageDefinitionBuilder, CancellableStage {
+  public static final String STAGE_TYPE = "canaryDisableCluster"
+  @Autowired CanaryStage canaryStage
+  @Autowired WaitStage waitStage
+  @Autowired CanaryDestroyClusterStage canaryDestroyClusterStage
+
+  @Override
+  void taskGraph(@Nonnull StageExecution stage, @Nonnull TaskNode.Builder builder) {
+    builder
+        .withTask("disableCanaryCluster", DisableCanaryTask)
+        .withTask("monitorDisable", MonitorKatoTask)
+        .withTask("disableBaselineCluster", DisableCanaryTask)
+        .withTask("monitorDisable", MonitorKatoTask)
+  }
+
+  @Override
+  void afterStages(StageExecution parent, StageGraphBuilder graph) {
+    Integer waitTime = parent.context.clusterDisableWaitTime != null ? stage.context.clusterDisableWaitTime : DEFAULT_CLUSTER_DISABLE_WAIT_TIME
+    graph.append {
+      it.type = waitStage.type
+      it.name = "Disable Canary and Baseline"
+      it.context = ["waitTime": waitTime]
+    }
+    graph.append {
+      it.type = canaryDestroyClusterStage.type
+      it.name = "Tear down Canary and Baseline"
+      it.context = parent.context
+    }
+  }
+
+  @Override
+  Result cancel(StageExecution stage) {
+    StageExecution canaryStageInstance = stage.ancestors().find {
+      it.type == CanaryStage.PIPELINE_CONFIG_TYPE
+    }
+
+    if (!canaryStageInstance) {
+      throw new IllegalStateException("No upstream canary stage found (stageId: ${stage.id}, executionId: ${stage.execution.id})")
+    }
+
+    return canaryStage.cancel(canaryStageInstance)
+  }
+}

orca-mine/src/main/groovy/com/netflix/spinnaker/orca/mine/pipeline/MonitorCanaryStage.groovy
@@ -17,9 +17,11 @@
 package com.netflix.spinnaker.orca.mine.pipeline
 
 import com.netflix.spinnaker.orca.api.pipeline.CancellableStage
+import com.netflix.spinnaker.orca.api.pipeline.graph.StageGraphBuilder
 import com.netflix.spinnaker.orca.api.pipeline.models.StageExecution
 import com.netflix.spinnaker.orca.clouddriver.tasks.MonitorKatoTask
 import com.netflix.spinnaker.orca.clouddriver.tasks.servergroup.ServerGroupCacheForceRefreshTask
+import com.netflix.spinnaker.orca.kayenta.pipeline.CanaryDisableClusterStage
 import com.netflix.spinnaker.orca.mine.MineService
 import com.netflix.spinnaker.orca.mine.tasks.*
 import com.netflix.spinnaker.orca.api.pipeline.graph.StageDefinitionBuilder
@@ -37,22 +39,22 @@ class MonitorCanaryStage implements StageDefinitionBuilder, CancellableStage {
 
   @Autowired MineService mineService
   @Autowired CanaryStage canaryStage
+  @Autowired CanaryDisableClusterStage canaryDisableClusterStage
 
   @Override
   void taskGraph(@Nonnull StageExecution stage, @Nonnull TaskNode.Builder builder) {
     builder
       .withTask("registerCanary", RegisterCanaryTask)
       .withTask("monitorCanary", MonitorCanaryTask)
-      .withTask("disableCanaryCluster", DisableCanaryTask)
-      .withTask("monitorDisable", MonitorKatoTask)
-      .withTask("waitBeforeCleanup", WaitTask)
-      .withTask("disableBaselineCluster", DisableCanaryTask)
-      .withTask("monitorDisable", MonitorKatoTask)
-      .withTask("waitBeforeCleanup", WaitTask)
-      .withTask("forceCacheRefresh", ServerGroupCacheForceRefreshTask)
-      .withTask("cleanupCanary", CleanupCanaryTask)
-      .withTask("monitorCleanup", MonitorKatoTask)
-      .withTask("completeCanary", CompleteCanaryTask)
+  }
+
+  @Override
+  void afterStages(StageExecution parent, StageGraphBuilder graph) {
+    graph.append {
+      it.type = canaryDisableClusterStage.type
+      it.name = "Disable Canary and Baseline"
+      it.context = parent.context
+    }
   }
 
   @Override

orca-mine/src/test/groovy/com/netflix/spinnaker/orca/mine/pipeline/CanaryDestroyClusterStageSpec.groovy
@@ -1,0 +1,69 @@
+/*
+ * Copyright 2020 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netflix.spinnaker.orca.mine.pipeline
+
+import com.netflix.spinnaker.orca.api.pipeline.CancellableStage
+import com.netflix.spinnaker.orca.pipeline.model.PipelineExecutionImpl
+import com.netflix.spinnaker.orca.pipeline.model.StageExecutionImpl
+import spock.lang.Specification
+
+import static com.netflix.spinnaker.orca.test.model.ExecutionBuilder.pipeline
+import static com.netflix.spinnaker.orca.test.model.ExecutionBuilder.stage
+
+class CanaryDestroyClusterStageSpec extends Specification {
+
+  def "should propagate cancel upstream if canary registered and execution explicitly canceled"() {
+    given:
+    def pipeline = pipeline {
+      stage {
+        refId = "1"
+        type = CanaryStage.PIPELINE_CONFIG_TYPE
+      }
+    }
+    def canaryStage = pipeline.stageByRef("1")
+    def canaryStageBuilder = Mock(CanaryStage)
+    def destroyCanaryClusterStage = new CanaryDestroyClusterStage(canaryStage: canaryStageBuilder)
+    def stage = new StageExecutionImpl(pipeline, "pipelineStage", [
+        canary: [id: "canaryId"]
+    ])
+    stage.setRequisiteStageRefIds(["1"])
+
+    when:
+    stage.execution.canceled = true
+    def result = destroyCanaryClusterStage.cancel(stage)
+
+    then:
+    result.details == [:]
+    1 * canaryStageBuilder.cancel(canaryStage) >> {
+      new CancellableStage.Result(stage, [:])
+    }
+  }
+
+  def "should raise exception if no upstream canary stage found"() {
+    def destroyClusterStage = new CanaryDestroyClusterStage()
+    def stage = new StageExecutionImpl(PipelineExecutionImpl.newPipeline("orca"), "pipelineStage", [
+        canary: [id: "canaryId"]
+    ])
+
+    when:
+    stage.execution.canceled = true
+    destroyClusterStage.cancel(stage)
+
+    then:
+    thrown(IllegalStateException)
+  }
+}

orca-mine/src/test/groovy/com/netflix/spinnaker/orca/mine/pipeline/CanaryDisableClusterStageSpec.groovy
@@ -1,0 +1,70 @@
+/*
+ * Copyright 2020 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netflix.spinnaker.orca.mine.pipeline
+
+import com.netflix.spinnaker.orca.api.pipeline.CancellableStage
+import com.netflix.spinnaker.orca.kayenta.pipeline.CanaryDisableClusterStage
+import com.netflix.spinnaker.orca.pipeline.model.PipelineExecutionImpl
+import com.netflix.spinnaker.orca.pipeline.model.StageExecutionImpl
+import spock.lang.Specification
+
+import static com.netflix.spinnaker.orca.test.model.ExecutionBuilder.pipeline
+import static com.netflix.spinnaker.orca.test.model.ExecutionBuilder.stage
+
+class CanaryDisableClusterStageSpec extends Specification {
+
+  def "should propagate cancel upstream if canary registered and execution explicitly canceled"() {
+    given:
+    def pipeline = pipeline {
+      stage {
+        refId = "1"
+        type = CanaryStage.PIPELINE_CONFIG_TYPE
+      }
+    }
+    def canaryStage = pipeline.stageByRef("1")
+    def canaryStageBuilder = Mock(CanaryStage)
+    def destroyCanaryClusterStage = new CanaryDisableClusterStage(canaryStage: canaryStageBuilder)
+    def stage = new StageExecutionImpl(pipeline, "pipelineStage", [
+        canary: [id: "canaryId"]
+    ])
+    stage.setRequisiteStageRefIds(["1"])
+
+    when:
+    stage.execution.canceled = true
+    def result = destroyCanaryClusterStage.cancel(stage)
+
+    then:
+    result.details == [:]
+    1 * canaryStageBuilder.cancel(canaryStage) >> {
+      new CancellableStage.Result(stage, [:])
+    }
+  }
+
+  def "should raise exception if no upstream canary stage found"() {
+    def destroyClusterStage = new CanaryDisableClusterStage()
+    def stage = new StageExecutionImpl(PipelineExecutionImpl.newPipeline("orca"), "pipelineStage", [
+        canary: [id: "canaryId"]
+    ])
+
+    when:
+    stage.execution.canceled = true
+    destroyClusterStage.cancel(stage)
+
+    then:
+    thrown(IllegalStateException)
+  }
+}

spinnaker/orca

commit 074fe69cada06ce18bd536302e91b3f89078893a

fix(mine): fix regression from prior PR Fix a regression from https://github.com/spinnaker/orca/pull/3758/files where the `waittime` property was removed

orca-mine/src/main/groovy/com/netflix/spinnaker/orca/mine/tasks/DisableCanaryTask.groovy
@@ -45,7 +45,6 @@ class DisableCanaryTask extends AbstractCloudProviderAwareTask implements Task {
       if (canary.health?.health == 'UNHEALTHY' || stage.context.unhealthy != null) {
         // If unhealthy, already disabled in MonitorCanaryTask
         return TaskResult.builder(ExecutionStatus.SUCCEEDED).context([
-          waitTime  : waitTime,
           unhealthy : true
         ]).build()
       }