da62ac037aa7cd75c8bf1ed0d1d93c48da9de9db
Mark Vulfson <mvulfson@netflix.com>
2020-08-06 21:14:17 -0700

    fix(exceptions): fix various exception issues 1. Don't log task failures on `UserException` 2. Don't fail retrieving properties file if the build wasn't started

--- orca-queue/src/main/kotlin/com/netflix/spinnaker/orca/q/handler/RunTaskHandler.kt
+++ orca-queue/src/main/kotlin/com/netflix/spinnaker/orca/q/handler/RunTaskHandler.kt
@@ -19,6 +19,7 @@ package com.netflix.spinnaker.orca.q.handler
 import com.netflix.spectator.api.BasicTag
 import com.netflix.spectator.api.Registry
 import com.netflix.spinnaker.kork.dynamicconfig.DynamicConfigService
+import com.netflix.spinnaker.kork.exceptions.UserException
 import com.netflix.spinnaker.orca.TaskExecutionInterceptor
 import com.netflix.spinnaker.orca.TaskResolver
 import com.netflix.spinnaker.orca.api.pipeline.OverridableTimeoutRetryableTask
@@ -170,7 +171,11 @@ class RunTaskHandler(
               queue.push(CompleteTask(message, SUCCEEDED))
             } else {
               if (e !is TimeoutException) {
-                log.error("Error running ${message.taskType.simpleName} for ${message.executionType}[${message.executionId}]", e)
+                if (e is UserException) {
+                  log.warn("${message.taskType.simpleName} for ${message.executionType}[${message.executionId}] failed, likely due to user error", e)
+                } else {
+                  log.error("Error running ${message.taskType.simpleName} for ${message.executionType}[${message.executionId}]", e)
+                }
               }
 
               val status = stage.failureStatus(default = TERMINAL)
