7b80fd81394d0bd607248ffe89da49fdd3c5f348
Apoorv Mahajan <amahaja3@ncsu.edu>
2021-02-01 13:01:17 -0800

    feat(clouddriver): update how run job failures are processed.
    - add logic to provide aditional metadata when run job failures occur. Raise an exception when state == failed
    - do not fail on errors while getting property file contents if jobStatus == Failed as it masks the real error behind the job failure.
    - refactor WaitOnJObCompletion class to use constructor based dependency injection
    - add tests for the above changes.
    
    This PR is closely coupled with https://github.com/spinnaker/clouddriver/pull/5184

--- orca-clouddriver/src/test/java/com/netflix/spinnaker/orca/clouddriver/tasks/job/WaitOnJobCompletionTest.java
+++ orca-clouddriver/src/test/java/com/netflix/spinnaker/orca/clouddriver/tasks/job/WaitOnJobCompletionTest.java
@@ -16,7 +16,11 @@
 
 package com.netflix.spinnaker.orca.clouddriver.tasks.job;
 
+import static com.netflix.spinnaker.orca.TestUtils.getResource;
+import static com.netflix.spinnaker.orca.TestUtils.getResourceAsStream;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 
@@ -28,29 +32,61 @@ import com.netflix.spinnaker.orca.api.pipeline.TaskResult;
 import com.netflix.spinnaker.orca.api.pipeline.models.ExecutionStatus;
 import com.netflix.spinnaker.orca.api.pipeline.models.ExecutionType;
 import com.netflix.spinnaker.orca.clouddriver.KatoRestService;
+import com.netflix.spinnaker.orca.clouddriver.exception.JobFailedException;
 import com.netflix.spinnaker.orca.front50.Front50Service;
 import com.netflix.spinnaker.orca.front50.model.Application;
 import com.netflix.spinnaker.orca.pipeline.model.PipelineExecutionImpl;
 import com.netflix.spinnaker.orca.pipeline.model.StageExecutionImpl;
+import com.netflix.spinnaker.orca.pipeline.persistence.ExecutionRepository;
+import java.io.IOException;
+import java.io.InputStream;
 import java.time.Duration;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import org.apache.commons.io.IOUtils;
 import org.assertj.core.api.AssertionsForClassTypes;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.platform.runner.JUnitPlatform;
-import org.junit.runner.RunWith;
-import org.springframework.test.util.ReflectionTestUtils;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import retrofit.client.Response;
 import retrofit.mime.TypedByteArray;
 
-@RunWith(JUnitPlatform.class)
+@ExtendWith(MockitoExtension.class)
 public final class WaitOnJobCompletionTest {
+  @Mock private KatoRestService mockKatoRestService;
+  private ObjectMapper objectMapper;
+  private RetrySupport retrySupport;
+  @Mock private JobUtils mockJobUtils;
+  @Mock private ExecutionRepository mockExecutionRepository;
+
+  @Mock private Front50Service mockFront50Service;
+  WaitOnJobCompletion task;
+
+  @BeforeEach
+  public void setup() {
+    objectMapper = new ObjectMapper();
+    retrySupport = new RetrySupport();
+
+    task =
+        new WaitOnJobCompletion(
+            mockKatoRestService,
+            objectMapper,
+            retrySupport,
+            mockJobUtils,
+            mockFront50Service,
+            mockExecutionRepository);
+  }
+
   @Test
   void jobTimeoutSpecifiedByRunJobTask() {
     Duration duration = Duration.ofMinutes(10);
 
-    WaitOnJobCompletion task = new WaitOnJobCompletion();
     StageExecutionImpl myStage =
         createStageWithContext(ImmutableMap.of("jobRuntimeLimit", duration.toString()));
     assertThat(task.getDynamicTimeout(myStage))
@@ -63,18 +99,6 @@ public final class WaitOnJobCompletionTest {
 
   @Test
   void taskSearchJobByApplicationUsingContextApplication() {
-    KatoRestService mockKatoRestService = mock(KatoRestService.class);
-    Front50Service mockFront50Service = mock(Front50Service.class);
-    RetrySupport retrySupport = new RetrySupport();
-    ObjectMapper objectMapper = new ObjectMapper();
-
-    WaitOnJobCompletion task = new WaitOnJobCompletion();
-
-    ReflectionTestUtils.setField(task, "retrySupport", retrySupport);
-    ReflectionTestUtils.setField(task, "objectMapper", objectMapper);
-    ReflectionTestUtils.setField(task, "katoRestService", mockKatoRestService);
-    ReflectionTestUtils.setField(task, "front50Service", mockFront50Service);
-
     Response mockResponse =
         new Response(
             "test-url",
@@ -101,18 +125,6 @@ public final class WaitOnJobCompletionTest {
 
   @Test
   void taskSearchJobByApplicationUsingContextMoniker() {
-    KatoRestService mockKatoRestService = mock(KatoRestService.class);
-    RetrySupport retrySupport = new RetrySupport();
-    ObjectMapper objectMapper = new ObjectMapper();
-    Front50Service mockFront50Service = mock(Front50Service.class);
-
-    WaitOnJobCompletion task = new WaitOnJobCompletion();
-
-    ReflectionTestUtils.setField(task, "retrySupport", retrySupport);
-    ReflectionTestUtils.setField(task, "objectMapper", objectMapper);
-    ReflectionTestUtils.setField(task, "katoRestService", mockKatoRestService);
-    ReflectionTestUtils.setField(task, "front50Service", mockFront50Service);
-
     Response mockResponse =
         new Response(
             "test-url",
@@ -138,18 +150,6 @@ public final class WaitOnJobCompletionTest {
 
   @Test
   void taskSearchJobByApplicationUsingParsedName() {
-    KatoRestService mockKatoRestService = mock(KatoRestService.class);
-    RetrySupport retrySupport = new RetrySupport();
-    ObjectMapper objectMapper = new ObjectMapper();
-    Front50Service mockFront50Service = mock(Front50Service.class);
-
-    WaitOnJobCompletion task = new WaitOnJobCompletion();
-
-    ReflectionTestUtils.setField(task, "retrySupport", retrySupport);
-    ReflectionTestUtils.setField(task, "objectMapper", objectMapper);
-    ReflectionTestUtils.setField(task, "katoRestService", mockKatoRestService);
-    ReflectionTestUtils.setField(task, "front50Service", mockFront50Service);
-
     Response mockResponse =
         new Response(
             "test-url",
@@ -174,18 +174,6 @@ public final class WaitOnJobCompletionTest {
 
   @Test
   void taskSearchJobByApplicationUsingExecutionApp() {
-    KatoRestService mockKatoRestService = mock(KatoRestService.class);
-    RetrySupport retrySupport = new RetrySupport();
-    ObjectMapper objectMapper = new ObjectMapper();
-    Front50Service mockFront50Service = mock(Front50Service.class);
-
-    WaitOnJobCompletion task = new WaitOnJobCompletion();
-
-    ReflectionTestUtils.setField(task, "retrySupport", retrySupport);
-    ReflectionTestUtils.setField(task, "objectMapper", objectMapper);
-    ReflectionTestUtils.setField(task, "katoRestService", mockKatoRestService);
-    ReflectionTestUtils.setField(task, "front50Service", mockFront50Service);
-
     Response mockResponse =
         new Response(
             "test-url",
@@ -205,6 +193,150 @@ public final class WaitOnJobCompletionTest {
     AssertionsForClassTypes.assertThat(result.getStatus()).isEqualTo(ExecutionStatus.SUCCEEDED);
     verify(mockKatoRestService, times(1)).collectJob(eq("test-app"), any(), any(), any());
     verify(mockFront50Service, times(0)).get(any());
+  }
+
+  @DisplayName(
+      "parameterized test for checking if an exception is thrown when a run job fails, with or without a propertyFile")
+  @ParameterizedTest(name = "{index} ==> includePropertyFile = {0}")
+  @ValueSource(booleans = {true, false})
+  void testFailedJobErrorHandling(boolean includePropertyFile) throws IOException {
+    // setup
+    InputStream jobStatusInputStream =
+        getResourceAsStream("clouddriver/tasks/job/failed-runjob-status.json");
+
+    Response mockResponse =
+        new Response(
+            "test-url",
+            200,
+            "test-reason",
+            Collections.emptyList(),
+            new TypedByteArray("application/json", IOUtils.toByteArray(jobStatusInputStream)));
+
+    when(mockKatoRestService.collectJob(any(), any(), any(), any())).thenReturn(mockResponse);
+
+    Map<String, Object> stageContext = new HashMap<>();
+    if (includePropertyFile) {
+      stageContext =
+          getResource(
+              objectMapper,
+              "clouddriver/tasks/job/failed-runjob-stage-context-with-property-file.json",
+              Map.class);
+    } else {
+      stageContext =
+          getResource(
+              objectMapper, "clouddriver/tasks/job/failed-runjob-stage-context.json", Map.class);
+    }
+
+    StageExecutionImpl myStage = createStageWithContext(stageContext);
+
+    // when
+    JobFailedException thrown = assertThrows(JobFailedException.class, () -> task.execute(myStage));
+
+    // then
+    verify(mockKatoRestService, times(1))
+        .collectJob(eq("test-app"), eq("test-account"), eq("test"), eq("job testrep"));
+
+    if (includePropertyFile) {
+      verify(mockKatoRestService, times(1))
+          .getFileContents(
+              eq("test-app"), eq("test-account"), eq("test"), eq("job testrep"), eq("testrep"));
+    } else {
+      verify(mockKatoRestService, never())
+          .getFileContents(anyString(), anyString(), anyString(), anyString(), anyString());
+    }
+    verifyNoInteractions(mockFront50Service);
+
+    // check to validate that we update the execution details in the repository before raising an
+    // exception
+    verify(mockExecutionRepository, times(1)).storeStage(myStage);
+
+    assertTrue(
+        thrown
+            .getMessage()
+            .matches(
+                "Job: 'testrep' failed."
+                    + " Reason: BackoffLimitExceeded."
+                    + " Details: Job has reached the specified backoff limit."
+                    + " Additional Details: Pod: 'testrepvmfv2-l1-3fd3c0443d46e3ac-bgdzw' had errors.\n"
+                    + " Container: 'testrepvmfv2-l1' exited with code: 1.\n"
+                    + " Status: Error.\n"
+                    + " Logs: fatal error"));
+  }
+
+  @DisplayName(
+      "parameterized test for checking if an exception is thrown when a run job fails, with or without a propertyFile")
+  @ParameterizedTest(name = "{index} ==> isPropertyFileContentsEmpty = {0}")
+  @ValueSource(booleans = {true, false})
+  void testPropertyFileErrorHandlingWhenARunJobFailed(boolean isPropertyFileContentsEmpty)
+      throws IOException {
+    // setup
+    InputStream jobStatusInputStream =
+        getResourceAsStream("clouddriver/tasks/job/failed-runjob-status.json");
+
+    Response mockResponse =
+        new Response(
+            "test-url",
+            200,
+            "test-reason",
+            Collections.emptyList(),
+            new TypedByteArray("application/json", IOUtils.toByteArray(jobStatusInputStream)));
+
+    when(mockKatoRestService.collectJob(any(), any(), any(), any())).thenReturn(mockResponse);
+
+    Map<String, Object> propertyFileContents = new HashMap<>();
+    if (!isPropertyFileContentsEmpty) {
+      propertyFileContents.put("some key", "some value");
+    }
+
+    when(mockKatoRestService.getFileContents(
+            eq("test-app"), eq("test-account"), eq("test"), eq("job testrep"), eq("testrep")))
+        .thenReturn(propertyFileContents);
+
+    Map<String, Object> stageContext =
+        getResource(
+            objectMapper,
+            "clouddriver/tasks/job/failed-runjob-stage-context-with-property-file.json",
+            Map.class);
+    StageExecutionImpl myStage = createStageWithContext(stageContext);
+
+    // when
+    JobFailedException thrown = assertThrows(JobFailedException.class, () -> task.execute(myStage));
+
+    // then
+    verify(mockKatoRestService, times(1))
+        .collectJob(eq("test-app"), eq("test-account"), eq("test"), eq("job testrep"));
+
+    verify(mockKatoRestService, times(1))
+        .getFileContents(
+            eq("test-app"), eq("test-account"), eq("test"), eq("job testrep"), eq("testrep"));
+
+    verifyNoInteractions(mockFront50Service);
+
+    // check to validate that we update the execution details in the repository before raising an
+    // exception
+    verify(mockExecutionRepository, times(1)).storeStage(myStage);
+
+    // validate that depending on the response obtained from the getFileContents() call, we either
+    // set
+    // propertyFileContents in the stage context or not
+    if (isPropertyFileContentsEmpty) {
+      assertThat(myStage.getContext().containsKey("propertyFileContents")).isFalse();
+    } else {
+      assertThat(myStage.getContext().containsKey("propertyFileContents")).isTrue();
+      assertThat(myStage.getContext().get("propertyFileContents")).isEqualTo(propertyFileContents);
+    }
+
+    assertTrue(
+        thrown
+            .getMessage()
+            .matches(
+                "Job: 'testrep' failed."
+                    + " Reason: BackoffLimitExceeded."
+                    + " Details: Job has reached the specified backoff limit."
+                    + " Additional Details: Pod: 'testrepvmfv2-l1-3fd3c0443d46e3ac-bgdzw' had errors.\n"
+                    + " Container: 'testrepvmfv2-l1' exited with code: 1.\n"
+                    + " Status: Error.\n"
+                    + " Logs: fatal error"));
   }
 
   private StageExecutionImpl createStageWithContext(Map<String, ?> context) {
