a5494058f85c62a69c58d35230218d13ec123d40
chris smalley <csmalley@netflix.com>
2021-03-11 11:51:48 -0800

    Remove component

--- orca-clouddriver/src/main/groovy/com/netflix/spinnaker/orca/clouddriver/KatoService.java
+++ orca-clouddriver/src/main/groovy/com/netflix/spinnaker/orca/clouddriver/KatoService.java
@@ -18,10 +18,8 @@ import java.util.Map;
 import javax.annotation.Nonnull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
 import retrofit.client.Response;
 
-@Component
 public class KatoService {
 
   private final Logger log = LoggerFactory.getLogger(getClass());
