spinnaker/spinnaker

commit f04f16ceac67580b4d0629c08361f885867ace4d

perf(events): create execution events with a ref to the execution (#4076)
* perf(events): create execution events with a ref to the execution

This avoid loading things for the repository twice, e.g.:
- on a StartTask message, the handler loads the entire execution
- it then emits a TaskStarted event, which will load the entire execution again
- ... and so on for TaskComplete, StageStarted, StageComplete, etc.

On simple executions, it decreases the amount of executions fetched by 40%, which should translate to less load on the database and lower memory pressure on orca.

* fix(test): reset state in StartExecutionHandlerTest

The test reuses state between tests, and StartExecutionHandler now mutates the status of the execution so we need to reset it.

orca-queue/src/main/kotlin/com/netflix/spinnaker/orca/q/handler/CompleteExecutionHandler.kt
@@ -63,9 +63,9 @@ class CompleteExecutionHandler(
       } else {
         message.determineFinalStatus(execution) { status ->
           repository.updateStatus(execution.type, message.executionId, status)
-          publisher.publishEvent(
-            ExecutionComplete(this, message.executionType, message.executionId, status)
-          )
+          execution.status = status
+          publisher.publishEvent(ExecutionComplete(this, execution))
+
           registry.counter(
             completedId.withTags(
               "status", status.name,
@@ -75,7 +75,10 @@ class CompleteExecutionHandler(
             )
           ).increment()
           if (status != SUCCEEDED) {
-            execution.topLevelStages.filter { it.status == RUNNING }.forEach {
+            execution
+              .topLevelStages
+              .filter { it.status == RUNNING }
+              .forEach {
               queue.push(CancelStage(it))
             }
           }

orca-queue/src/main/kotlin/com/netflix/spinnaker/orca/q/handler/StartExecutionHandler.kt
@@ -91,19 +91,21 @@ class StartExecutionHandler(
       val initialStages = execution.initialStages()
       if (initialStages.isEmpty()) {
         log.warn("No initial stages found (executionId: ${execution.id})")
+        execution.status = TERMINAL
         repository.updateStatus(execution.type, execution.id, TERMINAL)
-        publisher.publishEvent(ExecutionComplete(this, execution.type, execution.id, TERMINAL))
+        publisher.publishEvent(ExecutionComplete(this, execution))
       } else {
+        execution.status = RUNNING
         repository.updateStatus(execution.type, execution.id, RUNNING)
         initialStages.forEach { queue.push(StartStage(it)) }
-        publisher.publishEvent(ExecutionStarted(this, execution.type, execution.id))
+        publisher.publishEvent(ExecutionStarted(this, execution))
       }
     }
   }
 
   private fun terminate(execution: PipelineExecution) {
     if (execution.status == CANCELED || execution.isCanceled) {
-      publisher.publishEvent(ExecutionComplete(this, execution.type, execution.id, execution.status))
+      publisher.publishEvent(ExecutionComplete(this, execution))
       execution.pipelineConfigId?.let {
         queue.push(StartWaitingExecutions(it, purgeQueue = !execution.isKeepWaitingPipelines))
       }

orca-queue/src/test/kotlin/com/netflix/spinnaker/orca/q/handler/StartExecutionHandlerTest.kt
@@ -390,6 +390,7 @@ object StartExecutionHandlerTest : SubjectSpek<StartExecutionHandler>({
         beforeGroup {
           pipeline.isLimitConcurrent = true
           runningPipeline.isLimitConcurrent = true
+          pipeline.status = NOT_STARTED
 
           whenever(
             repository.retrievePipelinesForPipelineConfigId(eq(configId), any())

orca-core/src/main/java/com/netflix/spinnaker/orca/events/ExecutionComplete.java
@@ -17,22 +17,19 @@
 package com.netflix.spinnaker.orca.events;
 
 import com.netflix.spinnaker.orca.api.pipeline.models.ExecutionStatus;
-import com.netflix.spinnaker.orca.api.pipeline.models.ExecutionType;
+import com.netflix.spinnaker.orca.api.pipeline.models.PipelineExecution;
 import javax.annotation.Nonnull;
+import lombok.Getter;
 
 public final class ExecutionComplete extends ExecutionEvent {
-  private final ExecutionStatus status;
+  @Getter private final PipelineExecution execution;
 
-  public ExecutionComplete(
-      @Nonnull Object source,
-      @Nonnull ExecutionType executionType,
-      @Nonnull String executionId,
-      @Nonnull ExecutionStatus status) {
-    super(source, executionType, executionId);
-    this.status = status;
+  public ExecutionComplete(@Nonnull Object source, @Nonnull PipelineExecution execution) {
+    super(source, execution.getType(), execution.getId());
+    this.execution = execution;
   }
 
   public @Nonnull ExecutionStatus getStatus() {
-    return status;
+    return execution.getStatus();
   }
 }

orca-core/src/main/java/com/netflix/spinnaker/orca/events/ExecutionListenerAdapter.java
@@ -17,8 +17,6 @@
 package com.netflix.spinnaker.orca.events;
 
 import com.netflix.spinnaker.kork.common.Header;
-import com.netflix.spinnaker.orca.api.pipeline.models.ExecutionStatus;
-import com.netflix.spinnaker.orca.api.pipeline.models.PipelineExecution;
 import com.netflix.spinnaker.orca.listeners.DefaultPersister;
 import com.netflix.spinnaker.orca.listeners.ExecutionListener;
 import com.netflix.spinnaker.orca.listeners.Persister;
@@ -28,14 +26,11 @@ import org.springframework.context.ApplicationListener;
 
 /** Adapts events emitted by the nu-orca queue to an old-style listener. */
 public final class ExecutionListenerAdapter implements ApplicationListener<ExecutionEvent> {
-
   private final ExecutionListener delegate;
-  private final ExecutionRepository repository;
   private final Persister persister;
 
   public ExecutionListenerAdapter(ExecutionListener delegate, ExecutionRepository repository) {
     this.delegate = delegate;
-    this.repository = repository;
     persister = new DefaultPersister(repository);
   }
 
@@ -54,15 +49,11 @@ public final class ExecutionListenerAdapter implements ApplicationListener<Execu
   }
 
   private void onExecutionStarted(ExecutionStarted event) {
-    delegate.beforeExecution(persister, executionFor(event));
+    delegate.beforeExecution(persister, event.getExecution());
   }
 
   private void onExecutionComplete(ExecutionComplete event) {
-    ExecutionStatus status = event.getStatus();
-    delegate.afterExecution(persister, executionFor(event), status, status.isSuccessful());
-  }
-
-  private PipelineExecution executionFor(ExecutionEvent event) {
-    return repository.retrieve(event.getExecutionType(), event.getExecutionId());
+    delegate.afterExecution(
+        persister, event.getExecution(), event.getStatus(), event.getStatus().isSuccessful());
   }
 }

orca-core/src/main/java/com/netflix/spinnaker/orca/events/ExecutionStarted.java
@@ -16,12 +16,15 @@
 
 package com.netflix.spinnaker.orca.events;
 
-import com.netflix.spinnaker.orca.api.pipeline.models.ExecutionType;
+import com.netflix.spinnaker.orca.api.pipeline.models.PipelineExecution;
 import javax.annotation.Nonnull;
+import lombok.Getter;
 
 public final class ExecutionStarted extends ExecutionEvent {
-  public ExecutionStarted(
-      @Nonnull Object source, @Nonnull ExecutionType executionType, @Nonnull String executionId) {
-    super(source, executionType, executionId);
+  @Getter private final PipelineExecution execution;
+
+  public ExecutionStarted(@Nonnull Object source, @Nonnull PipelineExecution execution) {
+    super(source, execution.getType(), execution.getId());
+    this.execution = execution;
   }
 }

orca-core/src/main/java/com/netflix/spinnaker/orca/events/StageComplete.java
@@ -17,55 +17,28 @@
 package com.netflix.spinnaker.orca.events;
 
 import com.netflix.spinnaker.orca.api.pipeline.models.ExecutionStatus;
-import com.netflix.spinnaker.orca.api.pipeline.models.ExecutionType;
 import com.netflix.spinnaker.orca.api.pipeline.models.StageExecution;
 import javax.annotation.Nonnull;
+import lombok.Getter;
 
 public final class StageComplete extends ExecutionEvent {
-  private final String stageId;
-  private final String stageType;
-  private final String stageName;
-  private final ExecutionStatus status;
-
-  public StageComplete(
-      @Nonnull Object source,
-      @Nonnull ExecutionType executionType,
-      @Nonnull String executionId,
-      @Nonnull String stageId,
-      @Nonnull String stageType,
-      @Nonnull String stageName,
-      @Nonnull ExecutionStatus status) {
-    super(source, executionType, executionId);
-    this.stageId = stageId;
-    this.stageType = stageType;
-    this.stageName = stageName;
-    this.status = status;
-  }
+  @Getter private final StageExecution stage;
 
   public StageComplete(@Nonnull Object source, @Nonnull StageExecution stage) {
-    this(
-        source,
-        stage.getExecution().getType(),
-        stage.getExecution().getId(),
-        stage.getId(),
-        stage.getType(),
-        stage.getName(),
-        stage.getStatus());
+    super(source, stage.getExecution().getType(), stage.getExecution().getId());
+
+    this.stage = stage;
   }
 
   public @Nonnull String getStageId() {
-    return stageId;
+    return stage.getId();
   }
 
   public @Nonnull String getStageType() {
-    return stageType;
-  }
-
-  public @Nonnull String getStageName() {
-    return stageName;
+    return stage.getType();
   }
 
   public @Nonnull ExecutionStatus getStatus() {
-    return status;
+    return stage.getStatus();
   }
 }

spinnaker/orca

commit 2fad7d6bc530e430b5be549507be48c3ab58084a

fix(events): update in-memory execution before firing events
This fixes a regression introduced in f04f16ceac67580b4d0629c08361f885867ace4d, which caused e.g. trigger.parentExecution.endTime to be missing from child pipelines.

Before:
- a handler invoked executionRepository.updateStatus(type, id, status)
- `executionRepository` would add fields like `endTime`, `canceled` and persist them
- (before f04f16ceac67580b4d0629c08361f885867ace4d) the application events would load the execution from the db and fire the echo event
- (after f04f16ceac67580b4d0629c08361f885867ace4d) the application events would fire the echo event based on the in-memory execution, which was not updated with fields like `endTime` or `canceled`

After this change:
- handlers have to invoke execution.updateStatus(newStatus) to update the in-memory execution
- handlers then invoke executionRepository.updateStatus(execution, status) to persist the in-memory execution
- we fire echo events based on the in-memory execution, which has been updated with the required fields

orca-api/src/main/java/com/netflix/spinnaker/orca/api/pipeline/models/PipelineExecution.java
@@ -141,6 +141,8 @@ public interface PipelineExecution {
 
   StageExecution stageByRef(String refId);
 
+  void updateStatus(@Nonnull ExecutionStatus terminal);
+
   class AuthenticationDetails {
 
     private String user;

orca-core-tck/src/main/groovy/com/netflix/spinnaker/orca/pipeline/persistence/PipelineExecutionRepositoryTck.groovy
@@ -428,7 +428,6 @@ abstract class PipelineExecutionRepositoryTck<T extends ExecutionRepository> ext
     when:
     repository().cancel(execution.type, execution.id)
 
-
     then:
     with(repository().retrieve(execution.type, execution.id)) {
       canceled

orca-core/src/main/java/com/netflix/spinnaker/orca/listeners/DefaultPersister.java
@@ -16,7 +16,6 @@
 
 package com.netflix.spinnaker.orca.listeners;
 
-import com.netflix.spinnaker.orca.api.pipeline.models.ExecutionStatus;
 import com.netflix.spinnaker.orca.api.pipeline.models.ExecutionType;
 import com.netflix.spinnaker.orca.api.pipeline.models.StageExecution;
 import com.netflix.spinnaker.orca.pipeline.persistence.ExecutionRepository;
@@ -36,11 +35,5 @@ public class DefaultPersister implements Persister {
   @Override
   public boolean isCanceled(ExecutionType type, String executionId) {
     return executionRepository.isCanceled(type, executionId);
-  }
-
-  @Override
-  public void updateStatus(
-      ExecutionType type, String executionId, ExecutionStatus executionStatus) {
-    executionRepository.updateStatus(type, executionId, executionStatus);
   }
 }

orca-core/src/main/java/com/netflix/spinnaker/orca/listeners/Persister.java
@@ -16,7 +16,6 @@
 
 package com.netflix.spinnaker.orca.listeners;
 
-import com.netflix.spinnaker.orca.api.pipeline.models.ExecutionStatus;
 import com.netflix.spinnaker.orca.api.pipeline.models.ExecutionType;
 import com.netflix.spinnaker.orca.api.pipeline.models.StageExecution;
 
@@ -24,6 +23,4 @@ public interface Persister {
   void save(StageExecution stage);
 
   boolean isCanceled(ExecutionType type, String executionId);
-
-  void updateStatus(ExecutionType type, String executionId, ExecutionStatus executionStatus);
 }

orca-core/src/main/java/com/netflix/spinnaker/orca/pipeline/ExecutionLauncher.java
@@ -16,6 +16,7 @@
 
 package com.netflix.spinnaker.orca.pipeline;
 
+import static com.netflix.spinnaker.orca.api.pipeline.models.ExecutionStatus.TERMINAL;
 import static com.netflix.spinnaker.orca.api.pipeline.models.ExecutionType.PIPELINE;
 import static java.lang.Boolean.parseBoolean;
 import static java.lang.String.format;
@@ -26,7 +27,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.netflix.spectator.api.Registry;
 import com.netflix.spinnaker.kork.exceptions.UserException;
 import com.netflix.spinnaker.kork.web.exceptions.ValidationException;
-import com.netflix.spinnaker.orca.api.pipeline.models.ExecutionStatus;
 import com.netflix.spinnaker.orca.api.pipeline.models.ExecutionType;
 import com.netflix.spinnaker.orca.api.pipeline.models.PipelineExecution;
 import com.netflix.spinnaker.orca.api.pipeline.models.Trigger;
@@ -161,7 +161,6 @@ public class ExecutionLauncher {
   private PipelineExecution handleStartupFailure(PipelineExecution execution, Throwable failure) {
     final String canceledBy = "system";
     String reason = "Failed on startup: " + failure.getMessage();
-    final ExecutionStatus status = ExecutionStatus.TERMINAL;
 
     if (failure instanceof ValidationException) {
       ValidationException validationException = (ValidationException) failure;
@@ -191,7 +190,8 @@ public class ExecutionLauncher {
     } else {
       log.error("Failed to start {} {}", execution.getType(), execution.getId(), failure);
     }
-    executionRepository.updateStatus(execution.getType(), execution.getId(), status);
+    execution.updateStatus(TERMINAL);
+    executionRepository.updateStatus(execution, TERMINAL);
     executionRepository.cancel(execution.getType(), execution.getId(), canceledBy, reason);
     return executionRepository.retrieve(execution.getType(), execution.getId());
   }

orca-core/src/main/java/com/netflix/spinnaker/orca/pipeline/model/PipelineExecutionImpl.java
@@ -17,8 +17,10 @@
 package com.netflix.spinnaker.orca.pipeline.model;
 
 import static com.netflix.spinnaker.orca.api.pipeline.models.ExecutionStatus.NOT_STARTED;
+import static com.netflix.spinnaker.orca.api.pipeline.models.ExecutionStatus.RUNNING;
 import static com.netflix.spinnaker.orca.api.pipeline.models.ExecutionType.ORCHESTRATION;
 import static com.netflix.spinnaker.orca.api.pipeline.models.ExecutionType.PIPELINE;
+import static java.lang.System.currentTimeMillis;
 import static java.util.stream.Collectors.toMap;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
@@ -235,6 +237,17 @@ public class PipelineExecutionImpl implements PipelineExecution, Serializable {
 
   public void setStatus(@Nonnull ExecutionStatus status) {
     this.status = status;
+  }
+
+  @Override
+  public void updateStatus(@Nonnull ExecutionStatus status) {
+    this.status = status;
+    if (status == RUNNING) {
+      canceled = false;
+      startTime = currentTimeMillis();
+    } else if (status.isComplete() && startTime != null) {
+      endTime = currentTimeMillis();
+    }
   }
 
   private AuthenticationDetails authentication;

orca-core/src/main/java/com/netflix/spinnaker/orca/pipeline/persistence/ExecutionRepository.java
@@ -61,6 +61,10 @@ public interface ExecutionRepository {
 
   boolean isCanceled(ExecutionType type, @Nonnull String id);
 
+  default void updateStatus(@Nonnull PipelineExecution execution, @Nonnull ExecutionStatus status) {
+    updateStatus(execution.getType(), execution.getId(), status);
+  }
+
   void updateStatus(ExecutionType type, @Nonnull String id, @Nonnull ExecutionStatus status);
 
   @Nonnull

orca-qos/src/main/kotlin/com/netflix/spinnaker/orca/qos/ExecutionPromoter.kt
@@ -79,7 +79,8 @@ class DefaultExecutionPromoter(
         .also { result ->
           result.candidates.forEach {
             log.info("Promoting execution {} for work: {}", value("executionId", it.id), result.reason)
-            executionRepository.updateStatus(it.type, it.id, NOT_STARTED)
+            it.updateStatus(NOT_STARTED)
+            executionRepository.updateStatus(it, NOT_STARTED)
             executionLauncher.start(it)
           }
           registry.counter(promotedId).increment(result.candidates.size.toLong())

orca-queue/src/main/kotlin/com/netflix/spinnaker/orca/q/handler/CompleteExecutionHandler.kt
@@ -62,8 +62,8 @@ class CompleteExecutionHandler(
         log.info("Execution ${execution.id} already completed with ${execution.status} status")
       } else {
         message.determineFinalStatus(execution) { status ->
-          repository.updateStatus(execution.type, message.executionId, status)
-          execution.status = status
+          execution.updateStatus(status)
+          repository.updateStatus(execution, status)
           publisher.publishEvent(ExecutionComplete(this, execution))
 
           registry.counter(

orca-queue/src/main/kotlin/com/netflix/spinnaker/orca/q/handler/RestartStageHandler.kt
@@ -60,7 +60,8 @@ class RestartStageHandler(
           topStage.addRestartDetails(message.user)
           topStage.reset()
           restartParentPipelineIfNeeded(message, topStage)
-          repository.updateStatus(topStage.execution.type, topStage.execution.id, RUNNING)
+          topStage.execution.updateStatus(RUNNING)
+          repository.updateStatus(topStage.execution, RUNNING)
           queue.push(StartStage(startMessage))
         }
       }

orca-queue/src/main/kotlin/com/netflix/spinnaker/orca/q/handler/StartExecutionHandler.kt
@@ -91,12 +91,12 @@ class StartExecutionHandler(
       val initialStages = execution.initialStages()
       if (initialStages.isEmpty()) {
         log.warn("No initial stages found (executionId: ${execution.id})")
-        execution.status = TERMINAL
-        repository.updateStatus(execution.type, execution.id, TERMINAL)
+        execution.updateStatus(TERMINAL)
+        repository.updateStatus(execution, TERMINAL)
         publisher.publishEvent(ExecutionComplete(this, execution))
       } else {
-        execution.status = RUNNING
-        repository.updateStatus(execution.type, execution.id, RUNNING)
+        execution.updateStatus(RUNNING)
+        repository.updateStatus(execution, RUNNING)
         initialStages.forEach { queue.push(StartStage(it)) }
         publisher.publishEvent(ExecutionStarted(this, execution))
       }

orca-sql/src/main/kotlin/com/netflix/spinnaker/orca/sql/pipeline/persistence/SqlExecutionRepository.kt
@@ -260,19 +260,26 @@ class SqlExecutionRepository(
     }
   }
 
+  override fun updateStatus(execution: PipelineExecution, status: ExecutionStatus) {
+    if (execution.status != status) {
+      throw SystemException("execution ${execution.id} with status ${execution.status} " +
+        "has not been updated with new status ${status}")
+    }
+
+    withPool(poolName) {
+      jooq.transactional {
+        storeExecutionInternal(it, execution)
+      }
+    }
+  }
+
   override fun updateStatus(type: ExecutionType, id: String, status: ExecutionStatus) {
     withPool(poolName) {
       jooq.transactional {
         selectExecution(it, type, id)
           ?.let { execution ->
-            execution.status = status
-            if (status == RUNNING) {
-              execution.isCanceled = false
-              execution.startTime = currentTimeMillis()
-            } else if (status.isComplete && execution.startTime != null) {
-              execution.endTime = currentTimeMillis()
-            }
-            storeExecutionInternal(it, execution)
+            execution.updateStatus(status)
+            updateStatus(execution, status)
           }
       }
     }

orca-api-tck/src/main/kotlin/com/netflix/spinnaker/orca/api/test/PipelineBuilder.kt
@@ -34,6 +34,7 @@ import java.lang.System.currentTimeMillis
 fun pipeline(init: PipelineExecution.() -> Unit = {}): PipelineExecution {
   val pipeline = PipelineExecutionImpl(PIPELINE, "covfefe")
   pipeline.trigger = DefaultTrigger("manual")
+  pipeline.startTime = currentTimeMillis()
   pipeline.buildTime = currentTimeMillis()
   pipeline.init()
   return pipeline

orca-qos/src/test/kotlin/com/netflix/spinnaker/orca/qos/DefaultExecutionPromoterTest.kt
@@ -74,8 +74,8 @@ class DefaultExecutionPromoterTest : SubjectSpek<DefaultExecutionPromoter>({
         subject.tick()
 
         it("promotes all policy-selected candidate executions via status update") {
-          verify(executionRepository).updateStatus(execution1.type, execution1.id, ExecutionStatus.NOT_STARTED)
-          verify(executionRepository).updateStatus(execution2.type, execution2.id, ExecutionStatus.NOT_STARTED)
+          verify(executionRepository).updateStatus(execution1, ExecutionStatus.NOT_STARTED)
+          verify(executionRepository).updateStatus(execution2, ExecutionStatus.NOT_STARTED)
         }
 
         it("starts the executions immediately") {

orca-queue/src/test/kotlin/com/netflix/spinnaker/orca/q/handler/CompleteExecutionHandlerTest.kt
@@ -91,7 +91,7 @@ object CompleteExecutionHandlerTest : SubjectSpek<CompleteExecutionHandler>({
       }
 
       it("updates the execution") {
-        verify(repository).updateStatus(message.executionType, message.executionId, stageStatus)
+        verify(repository).updateStatus(pipeline, stageStatus)
       }
 
       it("publishes an event") {
@@ -100,6 +100,7 @@ object CompleteExecutionHandlerTest : SubjectSpek<CompleteExecutionHandler>({
             assertThat(it.executionType).isEqualTo(pipeline.type)
             assertThat(it.executionId).isEqualTo(pipeline.id)
             assertThat(it.status).isEqualTo(stageStatus)
+            assertThat(it.execution.endTime).isNotNull()
           }
         )
       }
@@ -166,6 +167,7 @@ object CompleteExecutionHandlerTest : SubjectSpek<CompleteExecutionHandler>({
 
       it("waits for the other branch(es)") {
         verify(repository, never()).updateStatus(eq(PIPELINE), eq(pipeline.id), any())
+        verify(repository, never()).updateStatus(any(), any())
       }
 
       it("does not publish any events") {
@@ -208,7 +210,7 @@ object CompleteExecutionHandlerTest : SubjectSpek<CompleteExecutionHandler>({
       }
 
       it("updates the pipeline status") {
-        verify(repository).updateStatus(PIPELINE, pipeline.id, stageStatus)
+        verify(repository).updateStatus(pipeline, stageStatus)
       }
 
       it("publishes an event") {
@@ -259,7 +261,7 @@ object CompleteExecutionHandlerTest : SubjectSpek<CompleteExecutionHandler>({
     }
 
     it("updates the execution") {
-      verify(repository).updateStatus(PIPELINE, message.executionId, TERMINAL)
+      verify(repository).updateStatus(pipeline, TERMINAL)
     }
 
     it("publishes an event") {
@@ -307,7 +309,7 @@ object CompleteExecutionHandlerTest : SubjectSpek<CompleteExecutionHandler>({
     }
 
     it("updates the execution") {
-      verify(repository).updateStatus(PIPELINE, message.executionId, SUCCEEDED)
+      verify(repository).updateStatus(pipeline, SUCCEEDED)
     }
 
     it("publishes an event") {
@@ -362,6 +364,7 @@ object CompleteExecutionHandlerTest : SubjectSpek<CompleteExecutionHandler>({
 
     it("does not complete the execution") {
       verify(repository, never()).updateStatus(eq(PIPELINE), any(), any())
+      verify(repository, never()).updateStatus(any(), any())
     }
 
     it("publishes no events") {

orca-queue/src/test/kotlin/com/netflix/spinnaker/orca/q/handler/RestartStageHandlerTest.kt
@@ -250,7 +250,7 @@ object RestartStageHandlerTest : SubjectSpek<RestartStageHandler>({
       }
 
       it("marks the execution as running") {
-        verify(repository).updateStatus(PIPELINE, pipeline.id, RUNNING)
+        verify(repository).updateStatus(pipeline, RUNNING)
       }
 
       it("runs the stage") {

orca-queue/src/test/kotlin/com/netflix/spinnaker/orca/q/handler/StartExecutionHandlerTest.kt
@@ -93,7 +93,7 @@ object StartExecutionHandlerTest : SubjectSpek<StartExecutionHandler>({
       }
 
       it("marks the execution as running") {
-        verify(repository).updateStatus(PIPELINE, message.executionId, RUNNING)
+        verify(repository).updateStatus(pipeline, RUNNING)
       }
 
       it("starts the first stage") {
@@ -105,6 +105,7 @@ object StartExecutionHandlerTest : SubjectSpek<StartExecutionHandler>({
           check<ExecutionStarted> {
             assertThat(it.executionType).isEqualTo(message.executionType)
             assertThat(it.executionId).isEqualTo(message.executionId)
+            assertThat(it.execution.startTime).isNotNull()
           }
         )
       }
@@ -262,7 +263,7 @@ object StartExecutionHandlerTest : SubjectSpek<StartExecutionHandler>({
       }
 
       it("marks the execution as TERMINAL") {
-        verify(repository, times(1)).updateStatus(PIPELINE, pipeline.id, TERMINAL)
+        verify(repository, times(1)).updateStatus(pipeline, TERMINAL)
       }
 
       it("publishes an event with TERMINAL status") {
@@ -347,7 +348,7 @@ object StartExecutionHandlerTest : SubjectSpek<StartExecutionHandler>({
         }
 
         it("does not start the new pipeline") {
-          verify(repository, never()).updateStatus(PIPELINE, message.executionId, RUNNING)
+          verify(repository, never()).updateStatus(pipeline, RUNNING)
           verify(queue, never()).push(isA<StartStage>())
         }
 
@@ -381,7 +382,7 @@ object StartExecutionHandlerTest : SubjectSpek<StartExecutionHandler>({
         }
 
         it("starts the new pipeline") {
-          verify(repository).updateStatus(PIPELINE, message.executionId, RUNNING)
+          verify(repository).updateStatus(pipeline, RUNNING)
           verify(queue).push(isA<StartStage>())
         }
       }
@@ -408,7 +409,7 @@ object StartExecutionHandlerTest : SubjectSpek<StartExecutionHandler>({
         }
 
         it("starts the new pipeline") {
-          verify(repository).updateStatus(PIPELINE, message.executionId, RUNNING)
+          verify(repository).updateStatus(pipeline, RUNNING)
           verify(queue).push(isA<StartStage>())
         }
       }