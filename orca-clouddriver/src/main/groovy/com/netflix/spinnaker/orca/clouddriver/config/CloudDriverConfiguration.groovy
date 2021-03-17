ef43ff4d1944e58c54765c3389bd82568a5f4190
chris smalley <csmalley@netflix.com>
2021-03-16 21:57:48 -0700

    feat(api): Add OperationsRunner interface with KatoOperationsRunner implementation
    Implementations of OperationsRunner can be used to fork operations to different services (besides CloudDriver, for example).

--- orca-clouddriver/src/main/groovy/com/netflix/spinnaker/orca/clouddriver/config/CloudDriverConfiguration.groovy
+++ orca-clouddriver/src/main/groovy/com/netflix/spinnaker/orca/clouddriver/config/CloudDriverConfiguration.groovy
@@ -24,6 +24,7 @@ import com.netflix.spinnaker.kork.core.RetrySupport
 import com.netflix.spinnaker.kork.web.selector.DefaultServiceSelector
 import com.netflix.spinnaker.kork.web.selector.SelectableService
 import com.netflix.spinnaker.kork.web.selector.ServiceSelector
+import com.netflix.spinnaker.orca.api.operations.OperationsRunner
 import com.netflix.spinnaker.orca.clouddriver.*
 import com.netflix.spinnaker.orca.jackson.OrcaObjectMapper
 import com.netflix.spinnaker.orca.retrofit.RetrofitConfiguration
@@ -168,6 +169,11 @@ class CloudDriverConfiguration {
                           RetrySupport retrySupport,
                           ObjectMapper objectMapper) {
     return new KatoService(katoRestService, cloudDriverTaskStatusService, retrySupport, objectMapper)
+  }
+
+  @Bean
+  OperationsRunner katoOperationsService(KatoService katoService) {
+    return new KatoOperationsRunner(katoService)
   }
 
   @Bean
