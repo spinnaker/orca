06a4cb6f1df2cdfc8d73fad56fae26e8bdc8ba79
chris smalley <csmalley@netflix.com>
2021-03-17 13:18:11 -0700

    add ConditionalOnMissingBean

--- orca-clouddriver/src/main/groovy/com/netflix/spinnaker/orca/clouddriver/config/CloudDriverConfiguration.groovy
+++ orca-clouddriver/src/main/groovy/com/netflix/spinnaker/orca/clouddriver/config/CloudDriverConfiguration.groovy
@@ -172,6 +172,7 @@ class CloudDriverConfiguration {
   }
 
   @Bean
+  @ConditionalOnMissingBean(OperationsRunner.class)
   OperationsRunner katoOperationsService(KatoService katoService) {
     return new KatoOperationsRunner(katoService)
   }
