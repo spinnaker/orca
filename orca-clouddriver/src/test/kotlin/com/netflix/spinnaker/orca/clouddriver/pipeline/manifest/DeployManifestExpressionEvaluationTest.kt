b01ae1c793aa7cc1feda2419a5ad96308224ee18
Dan Peach <daniel.peach@armory.io>
2021-05-25 09:01:46 -0400

    add more tests

--- orca-clouddriver/src/test/kotlin/com/netflix/spinnaker/orca/clouddriver/pipeline/manifest/DeployManifestExpressionEvaluationTest.kt
+++ orca-clouddriver/src/test/kotlin/com/netflix/spinnaker/orca/clouddriver/pipeline/manifest/DeployManifestExpressionEvaluationTest.kt
@@ -16,6 +16,7 @@
 
 package com.netflix.spinnaker.orca.clouddriver.pipeline.manifest
 
+import com.netflix.spinnaker.orca.pipeline.expressions.PipelineExpressionEvaluator.SpelEvaluatorVersion
 import com.netflix.spinnaker.orca.api.test.pipeline
 import com.netflix.spinnaker.orca.api.test.stage
 import com.netflix.spinnaker.orca.pipeline.StageDefinitionBuilderFactory
@@ -28,11 +29,13 @@ import strikt.api.expectThat
 import strikt.assertions.isEqualTo
 
 class DeployManifestExpressionEvaluationTest : JUnit5Minutests {
+  private val defaultSpelVersion = SpelEvaluatorVersion.Default().key
+
   fun tests() = rootContext<Fixture> {
     fixture { Fixture() }
 
-    listOf("v3", "v4", "").forEach { spelVersion ->
-      test("should evaluate expression in manifest when SpEL ${if (spelVersion == "") "default version" else spelVersion} is enabled") {
+    listOf(defaultSpelVersion, "v4").forEach { spelVersion ->
+      test("should evaluate expression in manifest by default when SpEL $spelVersion is enabled") {
         val pipeline = pipeline {
           if (spelVersion != "") spelEvaluator = spelVersion
           stage {
@@ -57,6 +60,36 @@ class DeployManifestExpressionEvaluationTest : JUnit5Minutests {
           )
         )))
       }
+    }
+
+    // When the default SpEL version is bumped from v3 -> v4 this test will fail.
+    // At that point we should remove this test and add a test to verify that the
+    // default SpEL version handles the 'skipExpressionEvaluation' flag to protect against regressions in SpEL v5+.
+    test("should evaluate expressions even when 'skipExpressionEvaluation' context flag is enabled for default SpEL evaluator") {
+      val pipeline = pipeline {
+        spelEvaluator = defaultSpelVersion
+        stage {
+          refId = "1"
+          type = "deployManifest"
+          context["skipExpressionEvaluation"] = true
+          context["manifests"] = listOf(
+            mapOf(
+              "metadata" to mapOf(
+                "name" to "my-k8s-manifest",
+                "should-evaluate-to-literal-true" to "\${#toBoolean('true')}"
+              )
+            )
+          )
+        }
+      }
+      val deployStage = pipeline.stageByRef("1")
+
+      expectThat(deployStage.withMergedContext().context["manifests"]).isEqualTo(listOf(mapOf(
+        "metadata" to mapOf(
+          "name" to "my-k8s-manifest",
+          "should-evaluate-to-literal-true" to true
+        )
+      )))
     }
 
     test("should not evaluate expressions in manifests when 'skipExpressionEvaluation' context flag is enabled and SpEL evaluator is v4") {
@@ -86,9 +119,9 @@ class DeployManifestExpressionEvaluationTest : JUnit5Minutests {
       )))
     }
 
-    listOf("v3", "v4", "").forEach { spelVersion ->
+    listOf(defaultSpelVersion, "v4").forEach { spelVersion ->
       listOf(true, false).forEach { skipExpressionEvaluation ->
-        test("non-manifest context values should be evaluated when SpEL ${if (spelVersion == "") "default version" else spelVersion} is enabled and skipExpressionEvaluation=$skipExpressionEvaluation") {
+        test("non-manifest context values should be evaluated when SpEL $spelVersion is enabled and skipExpressionEvaluation=$skipExpressionEvaluation") {
           val pipeline = pipeline {
             if (spelVersion != "") spelEvaluator = spelVersion
             stage {
