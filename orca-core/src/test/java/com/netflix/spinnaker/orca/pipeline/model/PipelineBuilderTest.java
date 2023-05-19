ef189547d194deecdce79411e19e01a46451355e
David Byron <dbyron@salesforce.com>
2023-05-19 16:47:37 -0700

    test(core): demonstrate the current behavior when invoking some invalid pipelines

---
+++ orca-core/src/test/java/com/netflix/spinnaker/orca/pipeline/model/PipelineBuilderTest.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2023 Salesforce, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netflix.spinnaker.orca.pipeline.model;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+
+class PipelineBuilderTest {
+
+  @Test
+  void withStagesChecksForNull() {
+    PipelineBuilder pipelineBuilder = new PipelineBuilder("my-application");
+    // There's no exception, but also no indication that the caller is likely
+    // doing something wrong by passing null.
+    pipelineBuilder.withStages(null);
+  }
+
+  @Test
+  void withStagesChecksForType() {
+    PipelineBuilder pipelineBuilder = new PipelineBuilder("my-application");
+    Map<String, Object> stageWithoutType = new HashMap<>();
+    stageWithoutType.put("name", "my-pipeline-stage");
+    // NullPointerException is unfortunate since the input is bad.  It
+    // translates to a 500 http response code, so callers retry.
+    assertThrows(
+        NullPointerException.class, () -> pipelineBuilder.withStages(List.of(stageWithoutType)));
+  }
+}
