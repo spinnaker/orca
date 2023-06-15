b11373fe3acce12c7ad715fa22f09ed25199935f
mattgogerly <matt.gogerly@jpmchase.com>
2023-06-15 09:25:28 +0100

    chore(lambda): fix licenses

--- orca-clouddriver/src/main/java/com/netflix/spinnaker/orca/clouddriver/tasks/providers/aws/lambda/LambdaOutputTask.java
+++ orca-clouddriver/src/main/java/com/netflix/spinnaker/orca/clouddriver/tasks/providers/aws/lambda/LambdaOutputTask.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 Netflix, Inc.
+ * Copyright 2018 Amazon.com, Inc. or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
