a51f27b0c586d06c65da8e9d0cfd7dfa9a12ed53
christosarvanitis <christos.arvanitis@armory.io>
2024-04-01 15:04:02 +0300

    fix(explicitRollback): Add configurable timeout for serverGroup lookup from Clouddriver API

--- orca-clouddriver/src/main/java/com/netflix/spinnaker/orca/clouddriver/config/RollbackConfigurationProperties.java
+++ orca-clouddriver/src/main/java/com/netflix/spinnaker/orca/clouddriver/config/RollbackConfigurationProperties.java
@@ -16,7 +16,10 @@
 
 package com.netflix.spinnaker.orca.clouddriver.config;
 
-import lombok.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
