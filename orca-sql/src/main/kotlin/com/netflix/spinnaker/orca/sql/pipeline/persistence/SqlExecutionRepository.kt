53927e288a6603ed82db2585edebb5dc1399b2dd
kirangodishala <kiran@opsmx.io>
2024-11-20 23:25:22 +0530

    refactor(sql): remove forUpdate argument from SqlExecutionRepository.selectExecution
    since it's not used.

--- orca-sql/src/main/kotlin/com/netflix/spinnaker/orca/sql/pipeline/persistence/SqlExecutionRepository.kt
+++ orca-sql/src/main/kotlin/com/netflix/spinnaker/orca/sql/pipeline/persistence/SqlExecutionRepository.kt
@@ -1275,14 +1275,10 @@ import kotlin.collections.toTypedArray
   private fun selectExecution(
     ctx: DSLContext,
     type: ExecutionType,
-    id: String,
-    forUpdate: Boolean = false
+    id: String
   ): PipelineExecution? {
     withPool(poolName) {
       val select = ctx.selectExecution(type, compressionProperties).where(id.toWhereCondition())
-      if (forUpdate) {
-        select.forUpdate()
-      }
       return select.fetchExecution()
     }
   }
